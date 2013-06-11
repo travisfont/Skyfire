4af912bc14a563bc8ee21d1478a9279997ca1c2c
Travis Font <travis@webservices.us.com>
2013-06-11 16:09:59 -0400

    color abbreviation function

---
+++ functions/color_abbr.func.php
@@ -0,0 +1,23 @@
+<?php
+
+//abbreviations  and acronym
+function color_abbr($abbr)
+{
+	switch(strtoupper($abbr))
+	{
+		case 'BRO': return 'BROWN';
+		case 'BLK': return 'BLACK';
+		case 'GRY': return 'GREY';
+		case 'GRN': return 'GREEN';
+		case 'HAZ': return 'HAZEL';
+		case 'BLU': return 'BLUE';
+		default: return $abbr;
+	}
+}
+
+/*
+Reference:
+http://www.all-acronyms.com/tag/color/2
+http://www.abbreviations.com/acronyms/COLORS
+http://www.ul.com/global/eng/pages/offerings/industries/chemicals/plastics/abbreviations/
+*/
