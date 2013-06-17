dd9d7f6227774c40eb7dc022185444ad1e5d7662
Travis Font <travis@webservices.us.com>
2013-06-17 18:23:51 -0400

    new html function to create left / right div

---
+++ functions/html/create_div_table.func.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// because i hate create left / right div table from scratch
+
+function create_div_table($html, $width, $style = FALSE)
+{
+	if($style === FALSE)
+	{
+		$style = ' style="margin:0; padding:0;"';
+	}
+	else
+	{
+		$style = ' style="'.$style.'"';
+	}
+
+	$string .= '<div><div style="width:'.$width[0].'; float:left;">';
+	$string .= $html[0]
+	$string .= '</div><div style="width:'.$width[1].'; float:left; text-align:right;">';
+	$string .= $html[1]
+	$string .= '</div></div>';
+	return $string;
+}
+
+// ------------
+
+$html[0] = 'left';
+$html[1] = 'right';
+
+create_div_table($html, array('50%', '50%'));
