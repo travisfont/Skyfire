c6161c132c82d0e8959d29e2c6534aa33ff630ff
Travis Font <travis@webservices.us.com>
2013-06-19 13:08:16 -0400

    new string length cut off function

---
+++ functions/string_length_cutoff.func.php
@@ -0,0 +1,14 @@
+<?php
+
+function string_length_cutoff($string, $limit, $subtext = '...')
+{
+    return (strlen($string) > $limit) ? substr($string, 0, ($limit-strlen(subtext))).$subtext : $string;
+}
+
+// example usage:
+#echo string_length_cutoff('Michelle Lee Hammontree-Garcia', 26);
+
+// or (for custom substitution text
+#echo string_length_cutoff('Michelle Lee Hammontree-Garcia', 26, '..');
+
+?>
