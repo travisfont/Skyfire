c1481e47dd7ce79a7dafce3aa4fa8818d1a2fe24
Travis van der F. <travis.font@gmail.com>
2013-09-06 11:57:52 -0400

    Update curl.class.php

--- classes/curl.class.php
+++ classes/curl.class.php
@@ -10,13 +10,13 @@ class cURL
 
 	function cURL($cookies = TRUE, $cookie = 'cookies.txt', $compression = 'gzip', $proxy = '')
 	{
-		$this->headers[]		= 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg'; 
-		$this->headers[]		= 'Connection: Keep-Alive'; 
-		$this->headers[]		= 'Content-type: application/x-www-form-urlencoded;charset=UTF-8'; 
+		$this->headers[]	= 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg'; 
+		$this->headers[]	= 'Connection: Keep-Alive'; 
+		$this->headers[]	= 'Content-type: application/x-www-form-urlencoded;charset=UTF-8'; 
 		$this->user_agent 	= 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)'; 
 		$this->compression	= $compression; 
-		$this->proxy			= $proxy; 
-		$this->cookies			= $cookies; 
+		$this->proxy		= $proxy; 
+		$this->cookies		= $cookies; 
 
 		if($this->cookies == TRUE)
 		{
