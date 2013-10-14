d414e18cb25ceb125a72124dcad092d0d490d04d
Travis van der Font <travis.font@gmail.com>
2013-10-14 15:13:41 -0400

    function get_page_title

---
+++ functions/html/get_page_title.func.php
@@ -0,0 +1,18 @@
+<?php
+
+function get_page_title($url)
+{
+	$str = file_get_contents($url);
+	if (strlen($str) > 0)
+	{
+		preg_match("/\<title\>(.*)\<\/title\>/i", $str, $title);
+		return $title[1];
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+// example:
+#echo getTitle("http://www.ggogle.com");
