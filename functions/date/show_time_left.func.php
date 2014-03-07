654741151e69334f856b656666bbfcaeb920d59d
Travis van der F. <travis.font@gmail.com>
2014-03-07 11:38:44 -0500

    Update show_time_left.func.php

--- functions/date/show_time_left.func.php
+++ functions/date/show_time_left.func.php
@@ -3,15 +3,14 @@
 function show_time_left($dbdate)
 {
 	$end_time = strtotime($dbdate);
-
-	$diff = $end_time - time();
-	$days = ($diff - ($diff % 86400)) / 86400;
-	$diff = $diff - ($days * 86400);
-	$hours = ($diff - ($diff % 3600)) / 3600;
-	$diff = $diff - ($hours * 3600);
-	$minutes = ($diff - ($diff % 60)) / 60;
-	$diff = $diff - ($minutes * 60);
-	$seconds = ($diff - ($diff % 1)) / 1;
+	$diff     = $end_time - time();
+	$days     = ($diff - ($diff % 86400)) / 86400;
+	$diff     = $diff - ($days * 86400);
+	$hours    = ($diff - ($diff % 3600)) / 3600;
+	$diff     = $diff - ($hours * 3600);
+	$minutes  = ($diff - ($diff % 60)) / 60;
+	$diff     = $diff - ($minutes * 60);
+	$seconds  = ($diff - ($diff % 1)) / 1;
 
 	return $days . "d" . " " . $hours . "h" . " " . $minutes . "m" . " " . $seconds . "s";
 }
