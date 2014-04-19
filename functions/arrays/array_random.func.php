404139982db1b1fd4673f5de2a1fbe77375045d9
Travis van der Font <travis.font@gmail.com>
2014-04-18 23:14:51 -0400

    --

--- functions/arrays/array_random.func.php
+++ functions/arrays/array_random.func.php
@@ -2,13 +2,16 @@
 
 // returns random element(s) from array
 
-function array_random($arr, $num = 1)
+function array_random($arr, $amount = 1)
 {
     shuffle($arr);
     
     $r = array();
-    for ($i = 0; $i < $num; $i++) 
+    for ($i = 0; $i < $amount; $i++) 
 	{
         $r[] = $arr[$i];
     }
+	
+	return $r;
+}
   
