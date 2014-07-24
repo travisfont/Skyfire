9771f6f93af58e5975057b8ad331cdd40bd9b495
Travis van der Font <travis.font@gmail.com>
2014-07-24 20:58:53 +0200

    --

---
+++ classes/constants.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This class provides a set of constants representing the days of the week.
+ */
+class Days
+{
+    const SUN = 'Sunday';
+    const MON = 'Monday';
+    const TUE = 'Tuesday';
+    const WED = 'Wednesday';
+    const THU = 'Thursday';
+    const FRI = 'Friday';
+    const SAT = 'Saturday';
+}
+
+/**
+ * This class provides a set of constants representing the months of the year.
+ */
+class Months
+{
+    const JAN = 'January';
+    const FEB = 'February';
+    const MAR = 'March';
+    const APR = 'April';
+    const MAY = 'May';
+    const JUN = 'June';
+    const JUL = 'July';
+    const AUG = 'August';
+    const SEP = 'September';
+    const OCT = 'October';
+    const NOV = 'November';
+    const DEC = 'December';
+}
