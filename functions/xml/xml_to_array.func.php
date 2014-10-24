61f34a59ad3ccc43f8f703660b67ad7459259b70
Travis van der Font <travis.font@gmail.com>
2014-10-24 16:04:25 +0200

    --

--- functions/xml/xml_to_array.func.php
+++ functions/xml/xml_to_array.func.php
@@ -1,6 +1,18 @@
 <?php
 
-function xml_to_array()
+// alias function
+function xml2array($xml_object)
 {
+    return xml_to_array($xml_object);
+}
 
-)
+// as a string
+function xml_to_array($xml_object)
+{
+    $out = array();
+    foreach ((array) $xml_object as $index => $node)
+    {
+        $out[$index] = (is_object($node)) ? xml_to_array($node) : $node;
+    }
+    return $out;
+) 
