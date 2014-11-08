bc0939d3169a1399ed2a88a94672db42c2f31042
Travis van der Font <travis.font@gmail.com>
2014-11-08 16:07:04 +0100

    --

--- classes/input.class.php
+++ classes/input.class.php
@@ -2,8 +2,14 @@
 
 class Input
 {
-
-	public static function post()
+    // if the field exist
+	public static function has($key)
+    {
+        return trim((string) $_POST[$key]) !== '';
+    }
+    
+    // retrieve all the post elements
+    public static function post()
 	{
 		if($_POST)
 		{
@@ -20,6 +26,7 @@ class Input
 		}
 	}
 	
+    // retrieve all the post elements
 	public static function get()
 	{
 		if($_GET)
@@ -36,8 +43,35 @@ class Input
 			return FALSE;
 		}
 	}
+    
+    // retrieve all the post and get elements
+    public static function all()
+	{
+        return array_merge(static::get(), static::post());
+    }
+    
+    // field validator
+    // @$rules - array
+    public static function validate($rules)
+    {
+    
+    }
+    
+    
 
 }
 
 // $data = Input::post();
 // $data['value'];
+
+/*
+$rules = array
+(
+    'file_name' => 'required',
+    'username ' => 'min:3',
+    'username ' => 'string',
+    'id'        => 'required',
+    'id'        => 'int', // alias: number, numeric
+    'desc'      => 'max:255'
+);
+Input::validate(); // if all the rules are met from the form it will return true, if not then a false.
