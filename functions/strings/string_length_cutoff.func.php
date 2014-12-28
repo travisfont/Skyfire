285fe6d0c4a11acbfd9607282adcff27ff256b4e
Travis van der F. <travis.font@gmail.com>
2014-12-28 14:50:57 +0100

    Update string_length_cutoff.func.php

--- functions/strings/string_length_cutoff.func.php
+++ functions/strings/string_length_cutoff.func.php
@@ -1,5 +1,7 @@
 <?php
 
+// Strings::length_cutoff()
+
 function string_length_cutoff($string, $limit, $subtext = '...')
 {
     return (strlen($string) > $limit) ? substr($string, 0, ($limit-strlen($subtext))).$subtext : $string;
