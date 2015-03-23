ff8b9b4f3edd2d9fd527aa2cecb8cbf5a7877e22
Travis van der Font <travis.font@gmail.com>
2015-03-23 09:52:22 +0100

    --

--- functions/strings/str_to_slug.func.php
+++ functions/strings/str_to_slug.func.php
@@ -1,8 +1,28 @@
-<?php
+<?php 
 
-function str_to_slug($string)
+function str_to_slug($text, $strict = FALSE)
 {
-    return trim($preg_replace('/-+/', "-", preg_replace('/[^a-z0-9-]/', '-', strtolower(trim($string)))),"-");
-}
+    $text = html_entity_decode($text, ENT_QUOTES, 'UTF-8');
 
-// $slug = str_to_slug('This is my example title'); 
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\\pL\d.]+~u', '-', $text);
+
+    // trim
+    $text = trim($text, '-');
+    setlocale(LC_CTYPE, 'en_GB.utf8');
+
+    // transliterate
+    if (function_exists('iconv'))
+    {
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    }
+
+    // remove unwanted characters + lowercase
+    $text = preg_replace('~[^-\w.]+~', '', strtolower($text));
+
+    if ($strict)
+    {
+        $text = str_replace(".", "_", $text);
+    }
+    return $text;
+}
