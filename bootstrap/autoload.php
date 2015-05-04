89f951e9229da4d7f9be2c7413ddad29a73b0034
Travis van der Font <travis.font@gmail.com>
2015-05-04 18:47:06 +0200

    still testing

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -2,9 +2,11 @@
 
 require_once 'global.constants.php';
 require_once 'routes.php';
+
 // automatically scan and register the entire class folder
+// ....
 
-$routes = 'ss'; //file_get_contents('../routes.php');
-
-echo $routes; exit;
 // app routes
+require_once '../routes.php';
+
+
