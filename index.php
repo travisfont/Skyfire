2d8167e44b94badc20ede3897feaf57c12017b5f
Travis van der Font <travis.font@gmail.com>
2015-06-24 23:21:26 +0200

    remove spacing

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
 // replace this with a ini file in the config folder FOR config.autoload
 define('BASE_DIRECTORY', 'localhost/Skyfire');
 
-
 require_once 'system/config.autoload.php';
 require_once 'system/global.constants.php';
 require_once 'system/route.organizer.php';
 require_once 'system/loader.php';
-
-
 require_once 'system/bootstrap.php';
