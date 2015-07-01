7c76bd42cbc856080c773094e9862d3befed785a
Travis van der Font <travis.font@gmail.com>
2015-07-02 00:31:34 +0200

    roundDollar math function

---
+++ library/functions/math/roundDollar.func.php
@@ -0,0 +1,27 @@
+<?php
+
+function roundDollar($amount)
+{
+    (float) $amount = str_replace('$', '', $amount);
+
+    $pow   = pow(10, 2);
+    $ceil  = ceil($amount * $pow) / $pow;
+    $floor = floor($amount * $pow) / $pow;
+
+    $pow = pow(10, 3);
+
+    $diff_ceil  = $pow * ($ceil - $amount);
+    $diff_floor = $pow * ($amount - $floor) + ($amount < 0 ? -1 : 1);
+
+    if ($diff_ceil >= $diff_floor)
+    {
+        return $floor;
+    }
+    else
+    {
+        return $ceil;
+    }
+}
+
+// Example
+#var_dump(roundDollar(123.45689));
