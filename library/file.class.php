fa2cccd8a08423fb974e7e95819e516ee442cc9a
Travis van der Font <travis.font@gmail.com>
2015-07-08 14:34:50 +0200

    new file library class

---
+++ library/file.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class File
+{
+    static public function includeIfExist($path){}      // $this->includeIfExist()
+    static public function includ_onceIfExist($path){}  // $this->includ_onceIfExist()
+    static public function requireIfExist($path){}      // $this->requireIfExist()
+    static public function require_onceIfExist($path){} // $this->require_onceIfExist()
+}
