24624e55d22e0645f000e1e8a5326f726de290a7
Travis van der Font <travis.font@gmail.com>
2015-07-08 14:35:17 +0200

    string library update

--- library/strings.class.php
+++ library/strings.class.php
@@ -2,7 +2,7 @@
 
 // String interface and function into object inherence
 
-class Strings extends System
+class Strings extends Controller
 {
     protected function generatePassword($length = 9, $strength = 4)
     {
