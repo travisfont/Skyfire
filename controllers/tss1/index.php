0ff0f1f2198fcbf2e3ab0224e3d6c2c0d9b45527
Travis van der Font <travis.font@gmail.com>
2015-07-14 12:23:05 +0200

    testing

--- controllers/tss1/index.php
+++ controllers/tss1/index.php
@@ -6,6 +6,9 @@ class Tss1 extends Controller
     {
         echo 'tss1 controller works';
 
+        load::library(SF::DEBUG);
+        ExecuteTime::start();
+
         $parameters = array
         (
             'key'      => '1e46165dsa5ds4a',
@@ -13,6 +16,10 @@ class Tss1 extends Controller
             'userid'   => 1234998,
             'keywords' => 'test'
         );
+
+        ExecuteTime::end();
+        print_r(ExecuteTime::display());
+
         /*
         Response::api('google')->with($parameters);
         Response::view('contact')->with($parameters);
