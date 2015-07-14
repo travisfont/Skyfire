2b653208d0c78ceb6e21bfad7ab1e2a8f24e399c
Travis van der Font <travis.font@gmail.com>
2015-07-14 12:22:32 +0200

    Update to SF global constants

--- system/global.constants.php
+++ system/global.constants.php
@@ -5,9 +5,18 @@ class SF
     const GET  = 'GET';
     const POST = 'POST';
 
-    const SYSTEM    = 'system';
+    const DEBUG     = 'debug';
+    const SYSLOG    = 'syslog';
     const CONSTANTS = 'constants';
+    const MATH      = 'math';
     const STRINGS   = 'strings';
     const ARRAYS    = 'arrays';
     const CSV       = 'csv';
+    const FILE      = 'file';
+    const OBJECTS   = 'objects';
+    const DATES     = 'dates';
+    const DATABASE  = 'database';
+    const CURL      = 'curl';
+    const TIME      = 'time';
+    const XML       = 'xml';
 }
