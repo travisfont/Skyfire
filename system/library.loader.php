ca878809f6dad3a0355a6f726f756dd2021c5d19
Travis van der Font <travis.font@gmail.com>
2015-07-28 17:51:09 +0200

    expender function loader

--- system/library.loader.php
+++ system/library.loader.php
@@ -1,10 +1,21 @@
 <?php
 
+class extender
+{
+    public function func($function_name)
+    {
+        /// load the seperate function
+    }
+}
+
+// System Autoloader
 class load
 {
     public static function library($class)
     {
         require_once '/library/'.strtolower($class).'.class.php';
+
+        return new extender();
     }
 
     public static function service($class)
@@ -12,3 +23,6 @@ class load
         require_once '/library/services/'.strtolower($class).'.php';
     }
 }
+
+// Autoloading
+// http://php.net/manual/en/language.oop5.autoload.php
