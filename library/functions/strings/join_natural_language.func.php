5dd6ad3d6dcced752737438ab930e58d35fc4377
Travis van der Font <travis.font@gmail.com>
2015-09-07 12:06:35 +0200

    -

---
+++ library/functions/strings/join_natural_language.func.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Join a string with a natural language conjunction at the end.
+ */
+function join_natural_language(array $list, $conjunction = 'and')
+{
+    $last = array_pop($list);
+
+    if ($list)
+    {
+        return implode(', ', $list).' '.$conjunction.' '.$last;
+    }
+
+    return $last;
+}
+
+/*
+// null
+var_dump(join_natural_language(array()));
+// string 'one'
+var_dump(join_natural_language(array('one')));
+// string 'one and two'
+var_dump(join_natural_language(array('one', 'two')));
+// string 'one, two and three'
+var_dump(join_natural_language(array('one', 'two', 'three')));
+// string 'one, two, three or four'
+var_dump(join_natural_language(array('one', 'two', 'three', 'four'), 'or'));
+ */
