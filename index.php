9656da9de3839b4881e1760ca36838a10866f6ac
Travis van der Font <travis.font@gmail.com>
2015-09-17 13:38:57 +0200

    -

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
+// defines the root directory of Skyfire
+define('ROOT_DIRECTORY', getcwd());
+
+// Exit early if running an incompatible PHP version to avoid fatal errors.
+if (version_compare(PHP_VERSION, '5.3.0') < 0)
+{
+    print 'Your PHP installation of version '.PHP_VERSION.' is too old and unsupported by Skyfire.';
+    exit;
+}
+
+// sysem files (all require to load)
 $system_files = array
 (
     'config.autoload',
--- system/controller.loader.php
+++ system/controller.loader.php
@@ -2,13 +2,108 @@
 
 /* Main Skyfire System Library */
 
+class ResponseStatusCode
+{
+    public function statusCode($code)
+    {
+
+    }
+}
+
+class ResponseWtih
+{
+
+    public function with(array $data)
+    {
+        return new ResponseStatusCode;
+    }
+}
+
 abstract class Response
 {
-    protected static function api($service_name) {}
-    protected static function view($view_name) {}
-    protected static function json() {}
-    protected static function xml() {}
-    protected function with($data) {}
+    protected function api($service_name) {}
+    protected function json()
+    {
+        return new ResponseWtih;
+    }
+    protected function xml()
+    {
+        return new ResponseWtih;
+    }
+    public function redirect($path, $code)
+    {
+
+        /*
+         if you're running in CLI (redirects cannot happen and thus shouldn't exit())
+          or if your webserver is running PHP as a (F)CGI
+           (it needs a previously set Status header to properly redirect)
+         */
+        if (strncmp('cli', PHP_SAPI, 3) !== 0)
+        {
+            if (headers_sent() !== TRUE)
+            {
+                if (strlen(session_id()) > 0) // if using sessions
+                {
+                    session_regenerate_id(TRUE); // avoids session fixation attacks
+                    session_write_close(); // avoids having sessions lock other requests
+                }
+
+                if (strncmp('cgi', PHP_SAPI, 3) === 0)
+                {
+                    header(sprintf('Status: %03u', $code), TRUE, $code);
+                }
+
+                header('Location: '.$path, TRUE, (preg_match('~^30[1237]$~', $code) > 0) ? $code : 302);
+            }
+
+            exit();
+        }
+        /*
+         301 - Moved Permanently
+        302 - Found
+        303 - See Other
+        307 - Temporary Redirect (HTTP/1.1)
+         */
+
+        return new ResponseWtih;
+    }
+    protected function view($view_name)
+    {
+        return new ResponseWtih;
+    }
+
+    /**
+     * Returns the equivalent of Apache's $_SERVER['REQUEST_URI'] variable.
+     *
+     * Because $_SERVER['REQUEST_URI'] is only available on Apache serves,
+     * this equivalent is using other environment variables.
+     */
+    protected function uri()
+    {
+        if (isset($_SERVER['REQUEST_URI']))
+        {
+            $uri = $_SERVER['REQUEST_URI'];
+        }
+        else
+        {
+            if (isset($_SERVER['argv']))
+            {
+                $uri = $_SERVER['SCRIPT_NAME'].'?'.$_SERVER['argv'][0];
+            }
+            elseif (isset($_SERVER['QUERY_STRING']))
+            {
+                $uri = $_SERVER['SCRIPT_NAME'].'?'.$_SERVER['QUERY_STRING'];
+            }
+            else
+            {
+                $uri = $_SERVER['SCRIPT_NAME'];
+            }
+        }
+
+        // prevent multiple slashes to avoid cross site requests via the Form API.
+        return '/'.ltrim($uri, '/');
+    }
+
 }
 
 class Controller extends Response
@@ -33,6 +128,7 @@ class Controller extends Response
             }
             else
             {
+                // might not be supported in PHP 5.3
                 return $instance->$function();
             }
         }
