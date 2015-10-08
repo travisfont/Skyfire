35673f81418f3dd56ab4870682531c489d7887e3
Travis van der Font <travis.font@gmail.com>
2015-10-08 11:09:06 +0200

    adding Stringy as a STRING service

---
+++ library/services/STRING/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Stringy - PHP String layer using Stringy for Skyfire
+ */
+
+use Stringy\Stringy as Stringy;
+
+require_once 'Stringy/src/Stringy.php';
+
+final class S extends Stringy {}
