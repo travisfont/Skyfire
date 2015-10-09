62b20deb2dec1d31d9c4bbd0a493266f7606fc4e
Travis van der Font <travis.font@gmail.com>
2015-10-09 09:33:48 +0200

    no longer supporting PHP 5.3 + adding Whoops support

--- public/index.php
+++ public/index.php
@@ -3,32 +3,40 @@
 // defines the root directory of Skyfire
 define('ROOT_DIRECTORY',   getcwd());
 define('PARENT_DIRECTORY', dirname(getcwd()));
+define('WHOOPS_DIRECTORY', '../library/packages/whoops/src/');
 
-// Exit early if running an incompatible PHP version to avoid fatal errors (at least PHP 5.3).
-if (version_compare(PHP_VERSION, '5.3.10') < 0)
+// TODO: eventually move this into the system process
+spl_autoload_register(function ($class)
+{
+    if (strstr($class, '\\', TRUE) == 'Whoops');
+    {
+        require_once WHOOPS_DIRECTORY.$class.'.php';
+    }
+});
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+// Exit early if running an incompatible PHP version to avoid fatal errors (at least PHP 5.4).
+if (version_compare(PHP_VERSION, '5.4.0') < 0)
 {
     print 'Your PHP installation of version '.PHP_VERSION.' is too old and unsupported by Skyfire.';
     exit;
 }
 else
 {
-    // if running a version before PHP 5.4 but after 5.3
-    if (version_compare(PHP_VERSION, '5.4.0') < 0)
+    // sysem files (all require to load)
+    foreach (array
+    (
+        'config.autoload',
+        'global.constants',
+        'route.organizer',
+        'library.loader',
+        'controller.loader',
+        'bootstrap',
+    ) as $file)
     {
-        require_once PARENT_DIRECTORY.'/system/php.5.4.0.php';
+        require_once PARENT_DIRECTORY.'/system/'.$file.'.php';
     }
-}
-
-// sysem files (all require to load)
-foreach (array
-(
-    'config.autoload',
-    'global.constants',
-    'route.organizer',
-    'library.loader',
-    'controller.loader',
-    'bootstrap',
-) as $file)
-{
-    require_once PARENT_DIRECTORY.'/system/'.$file.'.php';
 }
