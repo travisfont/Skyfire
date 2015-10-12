b595d1c24a9007884d1dedb145a765696d79a86c
Travis van der Font <travis.font@gmail.com>
2015-10-12 11:04:47 +0200

    Carbon for DateTime service

---
+++ library/services/DATETIME/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Carbon - PHP DAteTime layer using Carbon for Skyfire
+ */
+
+use Carbon\Carbon as _Carbon;
+
+require_once 'Carbon/src/Carbon/Carbon.php';
+
+final class Carbon extends _Carbon {}
