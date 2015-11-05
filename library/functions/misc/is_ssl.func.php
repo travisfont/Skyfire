f25fc513691cd92467b5e1d217f70fe6c97a74b3
Travis van der Font <travis.font@gmail.com>
2015-11-05 17:12:25 +0100

    add is_ssl()

---
+++ library/functions/misc/is_ssl.func.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Determine if SSL is used.
+ *
+ * @return bool True if SSL, false if not used.
+ */
+
+function is_ssl()
+{
+    if (isset($_SERVER['HTTPS']))
+    {
+        if (strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1')
+        {
+            return TRUE;
+        }
+    }
+    elseif (isset($_SERVER['SERVER_PORT']) && ($_SERVER['SERVER_PORT'] == '443'))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
