1fe6cc1d9d12a0ce84a1640f531e62bda7e2013c
Travis van der Font <travis.font@gmail.com>
2015-11-15 18:01:12 +0100

    refactor style parameters + strings (fix on comments to avoid parse error)

--- library/functions/html/create_div_table.func.php
+++ library/functions/html/create_div_table.func.php
@@ -5,26 +5,27 @@
 
 function create_div_table($html, $width, $style = FALSE)
 {
-	if($style === FALSE)
+	if ($style)
 	{
-		$style = ' style="margin:0; padding:0;"';
+        $style = 'style="'.$style.'"';
 	}
 	else
 	{
-		$style = ' style="'.$style.'"';
+        $style = 'style="margin:0; padding:0;"';
 	}
 
-	$string .= '<div><div style="width:'.$width[0].'; float:left;">';
-	$string .= $html[0]
+	$string  = '<div '.$style.'><div style="width:'.$width[0].'; float:left;">';
+	$string .= $html[0];
 	$string .= '</div><div style="width:'.$width[1].'; float:left; text-align:right;">';
-	$string .= $html[1]
+	$string .= $html[1];
 	$string .= '</div></div>';
+
 	return $string;
 }
 
-// ------------
-
+/*
 $html[0] = 'left';
 $html[1] = 'right';
 
 create_div_table($html, array('50%', '50%'));
+*/
