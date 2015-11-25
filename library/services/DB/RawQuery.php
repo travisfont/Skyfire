c0bf7ce86a008c7aa250f7eacd5462125e25c33a
Travis van der Font <travis.font@gmail.com>
2015-11-25 15:37:37 +0100

    Stash Queries

--- library/services/DB/RawQuery.php
+++ library/services/DB/RawQuery.php
@@ -2,5 +2,51 @@
 
 class RawQuery extends DB_Connector
 {
-    public function execute() {}
+    private $statement;
+    private $parameters;
+
+    public function __construct($statement, $parameters = NULL)
+    {
+        $this->statement  = $statement;
+
+        if (!empty($parameters))
+        {
+            $this->parameters = $parameters;
+        }
+    }
+
+    public function execute()
+    {
+        try
+        {
+            // checking and establish a live db connector
+            if (empty(self::$db))
+            {
+                self::$db = self::connect();
+            }
+
+            $stmt = self::$db->prepare($this->statement, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+            if ($stmt->execute($this->parameters))
+            {
+                // allow multiple queries
+                if (strlen(strstr($this->statement, ';')) > 1)
+                {
+                    $stmt->nextRowset();
+                }
+
+                //return $stmt->fetchAll(PDO::FETCH_CLASS);
+                $data = array();
+                foreach (new LastIterator(new DbRowIterator($stmt)) as $row)
+                {
+                    $data[] = $row;
+                }
+
+                return $data;
+            }
+        }
+        catch (PDOException $exception)
+        {
+            echo self::PDOException($exception, self::DISPLAY_TEXT);
+        }
+    }
 }
--- library/services/DB/StashQueries.php
+++ library/services/DB/StashQueries.php
@@ -36,9 +36,9 @@ final class DB extends DB_Connector
         return mb_convert_encoding($text, "HTML-ENTITIES", 'UTF-8');
     }
 
-    public static function query($statement, array $parameters = array(NULL))
+    public static function query($statement, array $parameters = NULL)
     {
-        return new RawQuery;
+        return new RawQuery($statement, $parameters);
     }
 
     public static function select($sql_file)
@@ -49,6 +49,29 @@ final class DB extends DB_Connector
     public static function update($sql_file)
     {
         return new ProcessQuery($sql_file, 'update');
+    }
+
+    public static function drop($table_name) {}
+    public static function truncate($table_name) {}
+    public static function fullempty($database_name)
+    {
+        $sql  = 'SET FOREIGN_KEY_CHECKS=0;';
+        $sql .= 'SHOW FULL TABLES FROM `'.$database_name.'` WHERE table_type = \'BASE TABLE\';';
+        $sql .= 'SET FOREIGN_KEY_CHECKS=1;';
+
+        /* also to delete views, functions, and events */
+        // SELECT `TABLE_NAME` FROM `INFORMATION_SCHEMA`.`TABLES` WHERE `TABLE_SCHEMA` = 'database_name' AND `TABLE_TYPE` = 'VIEW';
+        // SELECT `SPECIFIC_NAME` FROM `INFORMATION_SCHEMA`.`ROUTINES` WHERE `ROUTINE_SCHEMA` = 'database_name' AND ROUTINE_TYPE = 'FUNCTION';
+        // SELECT `EVENT_NAME` FROM `INFORMATION_SCHEMA`.`EVENTS` WHERE `EVENT_SCHEMA` = 'database_name' ORDER BY EVENT_NAME;
+
+        $drop = 'SET FOREIGN_KEY_CHECKS=0;';
+        foreach (self::query($sql)->execute() as $table)
+        {
+            $drop .= 'DROP TABLE `'.$database_name.'`.`'.reset($table).'`;';
+        }
+        $drop .= 'SET FOREIGN_KEY_CHECKS=1;';
+
+        self::query($drop)->execute();
     }
 
     // this type needs to be a display type (e.g. DISPLAY_TEST)
