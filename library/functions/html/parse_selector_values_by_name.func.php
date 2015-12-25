d1f03a170453562c6bf47497b41be031454e1331
Travis van der Font <travis.font@gmail.com>
2015-12-25 13:06:20 +0100

    add parse_selector_values_by_name()

---
+++ library/functions/html/parse_selector_values_by_name.func.php
@@ -0,0 +1,25 @@
+<?php
+
+// retrieves all the values from a selector by it's name by parsing its HTML
+
+function parse_selector_values_by_name($name, $selector_html, $encoding = 'UTF-8')
+{
+    $dom = new DOMDocument('1.0', $encoding);
+    $dom->loadHTML($selector_html);
+
+    $xpath   = new DOMXpath($dom);
+    $options = $xpath->query("*/select/option");
+    $options = $xpath->query("*/select[@name='".$name."']/option");
+    $result  = array();
+
+    foreach ($options as $option)
+    {
+        $optionValue          = $option->getAttribute('value');
+        $optionContent        = $option->nodeValue;
+        $result[$optionValue] = $optionContent;
+    }
+
+    return $result;
+}
+
+//var_dump(parse_selector_values_by_name('advertiser[]', $html));
