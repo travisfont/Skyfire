19a34e6a65d46ab305299c7f31f9002edf68fe71
Travis van der Font <travis.font@gmail.com>
2015-12-25 13:04:52 +0100

    add is_select()

---
+++ library/functions/html/is_select.func.php
@@ -0,0 +1,18 @@
+<?php
+
+function is_select($html, $encoding = 'UTF-8')
+{
+    $dom = new DOMDocument('1.0', $encoding);
+    $dom->loadHTML($html);
+
+    if ($dom->getElementsByTagName('select')->item(0)->nodeName == 'select')
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+// if (is_select('html selector text here')) {}
