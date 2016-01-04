03eb5f253d598ae9cdccdf86270aa8cdbf024706
Travis van der Font <travis.font@gmail.com>
2016-01-04 19:55:36 +0100

    add int_min() to math library

---
+++ library/functions/math/int_min.func.php
@@ -0,0 +1,28 @@
+<?php
+
+// find the minimum value for a define bit signed or unsigned integer
+
+function int_min($datatype)
+{
+    switch ($datatype)
+    {
+        case 'int8':
+            return (int) -128;
+        case 'uint8':
+            return (int) 0;
+        case 'int16':
+            return (int) -32768;
+        case 'uint16':
+            return (int) 0;
+        case 'int32':
+            return (int) -8388608;
+        case 'uint32':
+            return (int) 0;
+        case 'int64':
+            return (int) -9223372036854775808;
+        case 'uint64':
+            return (int) 0;
+        default:
+            return (bool) FALSE;
+    }
+}
