e5ae1289bebda27c0c41085efe0e1d929990876a
Travis van der Font <travis.font@gmail.com>
2016-01-22 23:49:49 +0100

    add create_image_from_base64()

---
+++ library/functions/image/create_image_from_base64.php
@@ -0,0 +1,17 @@
+<?php
+
+function create_image_from_base64($data, $filename, $extension = 'jpg', $quality = 100)
+{
+    $image_data = base64_decode($data);
+    $source     = imagecreatefromstring($image_data);
+    if (imagejpeg($source, $filename.'.'.$extension, $quality))
+    {
+        imagedestroy($source);
+
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
