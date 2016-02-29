02a6eeb6a4faa081ef3cfffb922cc0b0c6ccc384
Travis van der Font <travis.font@gmail.com>
2016-02-29 19:18:32 +0100

    function updates on strict data type pattern

--- library/classes/csv.class.php
+++ library/classes/csv.class.php
@@ -11,25 +11,54 @@ class Csv extends Controller
      */
     protected function array_getcsv($data)
     {
-        return array_getcsv($data);
+        //return array_getcsv($data);
+        return (array) self::parameters(
+        [
+            'data' => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($data)
+        ->returning(DT::TYPE_ARRAY);
     }
 
     // CsvToArray
     protected function csv_to_array($data)
     {
-        return csv_to_array($data);
+        //return csv_to_array($data);
+        return (array) self::parameters(
+        [
+            'data' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($data)
+        ->returning(DT::TYPE_ARRAY);
     }
 
     // CsvToAssocArray
     protected function csv_to_assoc_array($data)
     {
-        return csv_to_assoc_array($data);
+        //return csv_to_assoc_array($data);
+        return (array) self::parameters(
+        [
+            'data' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($data)
+        ->returning(DT::TYPE_ARRAY);
     }
 
     // cvsFileToAssocArray
-    protected function cvs_file_to_assoc_array($filename, $delimiter)
+    protected function cvs_file_to_assoc_array($filename, $delimiter = ',')
     {
-        return cvs_file_to_assoc_array($filename, $delimiter = ',');
+        //return cvs_file_to_assoc_array($filename, $delimiter);
+        return self::parameters(
+        [
+            'filename'  => DT::STRING,
+            'delimiter' => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($filename, $delimiter)
+        ->returning([DT::TYPE_ARRAY, DT::BOOL]);
     }
 
 }
