db0644d5ce749b55f6cffa65c6969371c326d3f1
Travis van der Font <travis.font@gmail.com>
2016-03-21 16:42:36 +0100

    updating dates library into time library (with some new functions)

--- library/classes/time.class.php
+++ library/classes/time.class.php
@@ -1,11 +1,74 @@
-<?php 
+<?php
 
-class time
+// Time interface and function into object inherence
+
+class Time extends Controller
 {
+    protected function date_yesterday($str)
+    {
+        return date_yesterday($str);
+    }
+
+    protected function datetime_diff($date1, $date2)
+    {
+        return datetime_diff($date1, $date2);
+    }
+
+    protected function days_diff($date1, $date2 = FALSE, $accuracy_day = FALSE)
+    {
+        return days_diff($date1, $date2, $accuracy_day);
+    }
+
+    protected function months_diff($date1, $date2 = FALSE)
+    {
+        return months_diff($date1, $date2);
+    }
+
+    protected function show_time_left($dbdate)
+    {
+        return show_time_left($dbdate);
+    }
+
+    // timeAge
+    protected function time_ago($date)
+    {
+        return time_ago($date);
+    }
+
+    // ConvertUtcOffsetAbbr()
+    protected function convert_utc_offset_abbr($offset)
+    {
+        return convert_utc_offset_abbr($offset);
+    }
+
+    //nextWeek
+    protected function next_week($date, $weeks = 1, $format = 'Y-m-d\TH:i:sP')
+    {
+        return next_week($date, $weeks, $format);
+    }
+
+    // ideal naming convertion: MySQL_Datetime_Format
+
+    protected function format_mysql_datetime($date_string = FALSE)
+    {
+        return format_mysql_datetime($date_string);
+    }
+
+    // MysqlNow
+    protected function mysql_now()
+    {
+        return mysql_now();
+    }
+
+    protected function convert_month($number, array $args = array('lang' => 'fr', 'ucword' => FALSE, 'dots' => FALSE, 'long' => FALSE))
+    {
+        return convert_month($number, $args);
+    }
+
     public static function getUTCTimeZone()
     {
         $UTCDateTimeZone = new DateTimeZone('UTC');
-                    return new DateTime("now", $UTCDateTimeZone);
+        return new DateTime("now", $UTCDateTimeZone);
     }
 
     public static function serverTimeZone_offset($userTimeZone = 'UTC')
@@ -23,22 +86,22 @@ class time
     public static function getDefineTimeZone($timezone)
     {
         $userDateTimeZone = new DateTimeZone($timezone);
-                     return new DateTime("now", $userDateTimeZone);
+        return new DateTime("now", $userDateTimeZone);
     }
 
     public static function getServerTimeZone()
     {
         $serverTimeZone     = date_default_timezone_get();
         $serverDateTimeZone = new DateTimeZone($serverTimeZone);
-        
+
         return new DateTime("now", $serverDateTimeZone);
     }
 }
 
 /*
-$userDateTime   = time::getDefineTimeZone('America/Curacao');
-$serverDateTime = time::getServerTimeZone();
-$timeOffset     = time::gserverTimeZone_offset('America/Curacao');
+$userDateTime   = Time::getDefineTimeZone('America/Curacao');
+$serverDateTime = Time::getServerTimeZone();
+$timeOffset     = Time::gserverTimeZone_offset('America/Curacao');
 
 var_dump($userDateTime);
 var_dump($serverDateTime);
@@ -47,4 +110,3 @@ var_dump($timeOffset);
 $userDateTime->add(new DateInterval('PT'.$timeOffset.'S'));
 
 var_dump($userDateTime);
-*/
