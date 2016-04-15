71d9a9c0c4d3a7528241f876368dbe6ad7dc865c
Travis van der Font <travis.font@gmail.com>
2016-04-15 17:00:55 +0200

    type hint update on the return

--- library/classes/objects.class.php
+++ library/classes/objects.class.php
@@ -7,6 +7,15 @@ class Objects extends Controller
     // convertObjectsToArrays
     protected function convert_objects_to_arrays($obj, &$arr)
     {
-        return convert_objects_to_arrays($obj, $arr);
+        //return convert_objects_to_arrays($obj, $arr);
+
+        return (array) self::parameters(array
+        (
+            'obj' => DT::STD,
+            'arr' => DT::TYPE_ARRAY
+        ))
+        ->call(__FUNCTION__)
+        ->with($obj, $arr)
+        ->returning(DT::STRING);
     }
 }
