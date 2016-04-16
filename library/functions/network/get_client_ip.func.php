2bafcc2a3bcb9c1208286b87b81fa71b3426ff97
Travis van der Font <travis.font@gmail.com>
2016-04-16 15:10:00 +0200

    type hint update on the return

--- library/functions/network/get_client_ip.func.php
+++ library/functions/network/get_client_ip.func.php
@@ -4,14 +4,14 @@ function get_client_ip()
 {
     if (!empty($_SERVER['HTTP_CLIENT_IP']))
     {
-        return $_SERVER['HTTP_CLIENT_IP'];
+        return (string) $_SERVER['HTTP_CLIENT_IP'];
     }
     elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
     {
-        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        return (string) $_SERVER['HTTP_X_FORWARDED_FOR'];
     }
     else
     {
-        return $_SERVER['REMOTE_ADDR'];
+        return (string) $_SERVER['REMOTE_ADDR'];
     }
 }
