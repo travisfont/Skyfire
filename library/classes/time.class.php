bfcf534aa36ccfc9cbc9d2c2fe482a4ad33cf3a4
Travis van der Font <travis.font@gmail.com>
2016-05-29 22:33:09 +0200

    add default php datetime constants

--- library/classes/time.class.php
+++ library/classes/time.class.php
@@ -4,6 +4,18 @@
 
 class Time extends Controller
 {
+    const ATOM    = "Y-m-d\TH:i:sP";
+    const COOKIE  = "l, d-M-Y H:i:s T";
+    const ISO8601 = "Y-m-d\TH:i:sO";
+    const RFC822  = "D, d M y H:i:s O";
+    const RFC850  = "l, d-M-y H:i:s T";
+    const RFC1036 = "D, d M y H:i:s O";
+    const RFC1123 = "D, d M Y H:i:s O";
+    const RFC2822 = "D, d M Y H:i:s O";
+    const RFC3339 = "Y-m-d\TH:i:sP";
+    const RSS     = "D, d M Y H:i:s O";
+    const W3C     = "Y-m-d\TH:i:sP";
+
     protected function date_yesterday($str)
     {
         return date_yesterday($str);
@@ -96,6 +108,9 @@ class Time extends Controller
 
         return new DateTime("now", $serverDateTimeZone);
     }
+
+
+
 }
 
 /*
