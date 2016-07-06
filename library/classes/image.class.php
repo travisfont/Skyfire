9a5e7dfea134096c3cacb97cb33920ee6af2462d
Travis van der Font <travis.font@gmail.com>
2016-07-06 22:23:37 +0200

    Image library - function updates on strict data type pattern

--- library/classes/image.class.php
+++ library/classes/image.class.php
@@ -7,19 +7,44 @@ class Image extends Controller
     // darkerHex
     protected function darker_hex($hex, $amount = 30)
     {
-        return darker_hex($hex, $amount);
+        //return darker_hex($hex, $amount);
+        return self::parameters(
+        [
+            'hex'    => DT::STRING,
+            'amount' => DT::UINT8
+        ])
+        ->call(__FUNCTION__)
+        ->with($hex, $amount)
+        ->returning([DT::BOOL, DT::STRING]);
     }
 
     // getImageScaleSize
     protected function get_image_scale_size($filename, $max_w = 100, $max_h = NULL)
     {
-        return get_image_scale_size($filename, $max_w, $max_h);
+        //return get_image_scale_size($filename, $max_w, $max_h);
+        return self::parameters(
+        [
+            'filename' =>  DT::STRING,
+            'max_w'    =>  DT::UINT16,
+            'max_h'    => [DT::UINT16, DT::NULL]
+        ])
+        ->call(__FUNCTION__)
+        ->with($filename, $max_w, $max_h)
+        ->returning([DT::BOOL, DT::NULL, DT::STD]);
     }
 
     // getImageSizePercentage
     protected function get_image_size_percentage($filename, $percentage)
     {
-        return get_image_size_percentage($filename, $percentage);
+        //return get_image_size_percentage($filename, $percentage);
+        return self::parameters(
+        [
+            'filename'   =>  DT::STRING,
+            'percentage' =>  DT::UINT8,
+        ])
+        ->call(__FUNCTION__)
+        ->with($filename, $percentage)
+        ->returning([DT::BOOL, DT::NULL, DT::STD]);
     }
 
 
