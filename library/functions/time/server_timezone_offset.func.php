b3978860a5c8ae2b593a608c33b70580d78cbd64
Travis van der Font <travis.font@gmail.com>
2016-07-08 15:25:15 +0200

    add server_timezone_offset() to time library

---
+++ library/functions/time/server_timezone_offset.func.php
@@ -0,0 +1,13 @@
+<?php
+
+function server_timezone_offset($user_timezone = 'UTC')
+{
+    $user_datetimezone = new DateTimeZone($user_timezone);
+    $user_datetime     = new DateTime("now", $user_datetimezone);
+
+    $server_timezone     = date_default_timezone_get();
+    $server_datetimezone = new DateTimeZone($server_timezone);
+    $server_datetime     = new DateTime("now", $server_datetimezone);
+
+    return (int) ($server_datetimezone->getOffset($server_datetime) - $user_datetimezone->getOffset($user_datetime));
+}
