90deb29858327b2c16895678fefdacde619c745c
Travis van der Font <travis.font@gmail.com>
2016-08-04 11:06:52 +0200

    Camel Case CSV interface for IDE

--- library/classes/camelcase/csv.cc.class.php
+++ library/classes/camelcase/csv.cc.class.php
@@ -9,52 +9,14 @@ class Csv extends Controller
      * @return array
      */
     protected function arrayGetCsv($data)
-    {
-        //return array_getcsv($data);
-        return (array) self::parameters(
-        [
-            'data' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($data)
-        ->returning(DT::TYPE_ARRAY);
-    }
+    { return (array) NULL; }
 
     protected function CsvToArray($data)
-    {
-        //return csv_to_array($data);
-        return (array) self::parameters(
-        [
-            'data' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($data)
-        ->returning(DT::TYPE_ARRAY);
-    }
+    { return (array) NULL; }
 
     protected function CsvToAssocArray($data)
-    {
-        //return csv_to_assoc_array($data);
-        return (array) self::parameters(
-        [
-            'data' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($data)
-        ->returning(DT::TYPE_ARRAY);
-    }
+    { return (array) NULL; }
 
     protected function cvsFileToAssocArray($filename, $delimiter = ',')
-    {
-        //return cvs_file_to_assoc_array($filename, $delimiter);
-        return self::parameters(
-        [
-            'filename'  => DT::STRING,
-            'delimiter' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($filename, $delimiter)
-        ->returning([DT::TYPE_ARRAY, DT::BOOL]);
-    }
-
+    { return var_export(NULL); }
 }
