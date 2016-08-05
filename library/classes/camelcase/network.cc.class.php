d9fc0b5b63d097aed94026260113ab708ff585d9
Travis van der Font <travis.font@gmail.com>
2016-08-05 10:55:51 +0200

    Camel Case Network interface for IDE

--- library/classes/camelcase/network.cc.class.php
+++ library/classes/camelcase/network.cc.class.php
@@ -5,61 +5,20 @@
 class Network extends Controller
 {
     protected function getAddressCoordinates($address)
-    {
-        //return get_address_coordinates($address);
-        return self::parameters(
-        [
-            'address' =>  DT::STRING,
-        ])
-        ->call(__FUNCTION__)
-        ->with($address)
-        ->returning([DT::TYPE_ARRAY, DT::BOOL]);
-    }
+    { return var_export(NULL); }
 
     protected function getClientIp()
-    {
-        //return (string) get_client_ip();
-        return (string) self::parameters()->call(__FUNCTION__)
-                                          ->returning(DT::IPV4);
-    }
+    { return (string) NULL; }
 
     protected function isSsl()
-    {
-        //return (bool) is_ssl();
-        return (bool) self::parameters()->call(__FUNCTION__)
-                                        ->returning(DT::BOOL);
-    }
+    { return (bool) NULL; }
 
     protected function whois($domain, $registrar = FALSE)
-    {
-        //return whois($domain, $registrar);
-        return self::parameters(
-        [
-            'domain'    =>  DT::STRING,
-            'registrar' => [DT::STRING, DT::BOOL]
-        ])
-        ->call(__FUNCTION__)
-        ->with($domain, $registrar)
-        ->returning([DT::STRING, DT::BOOL]);
-    }
+    { return var_export(NULL); }
 
-    // TODO: THIS FUNCTION REQUIRES TESTING - VOID PARAMETERS
     protected function getClientLang()
-    {
-        //return get_client_lang();
-        return (string) self::parameters()->call(__FUNCTION__)
-                                          ->returning(DT::STRING);
-    }
+    { return (string) NULL; }
 
     protected function isEmail($string)
-    {
-        //return (bool) is_email($string);
-        return (bool) self::parameters(
-        [
-            'string' =>  DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($string)
-        ->returning(DT::BOOL);
-    }
+    { return (bool) NULL; }
 }
