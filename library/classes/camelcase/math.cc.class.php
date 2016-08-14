c73099c2830ef86eee87e3b979cd0413c2e82140
Travis van der Font <travis.font@gmail.com>
2016-08-14 20:41:03 +0200

    Camel Case Math interface for IDE

---
+++ library/classes/camelcase/math.cc.class.php
@@ -0,0 +1,57 @@
+<?php
+
+// Math interface and function into object inherence
+
+class Math extends Controller
+{
+    protected function luhn($number)
+    { return (bool) NULL; }
+
+    protected function randomizeCrypto($min, $max)
+    { return (int) NULL; }
+
+    protected function calculateAge($date, $format = 'm/d/Y')
+    { return (float) NULL; }
+
+    protected function calculateDistance($lat1, $lng1, $lat2, $lng2, $miles = FALSE)
+    { return (float) NULL; }
+
+    protected function roundDollar($amount)
+    { return (float) NULL; }
+
+    protected function formatCurrency($floatcurr, $curr = 'USD')
+    { return (string) NULL; }
+
+    protected function randomNumber($digits, $leading_zeros = FALSE)
+    { return (NULL) ? (int) NULL : (string) NULL; }
+
+    protected function getPercentageOf($percentage, $number)
+    { return (float) NULL; }
+
+    protected function leadingZero($num, $places = 0)
+    { return (string) NULL; }
+
+    protected function roundMins($hour, $minutes = 1, $format = 'H:i')
+    { return (NULL) ? (bool) NULL : (string) NULL; }
+
+    protected function savedPercentage($original_price, $current_price)
+    { return (string) NULL; }
+
+    protected function absint($number)
+    { return (int) NULL; }
+
+    protected function isEven($number)
+    { return (float) NULL; }
+
+    protected function isOdd($number)
+    { return (float) NULL; }
+
+    protected function intMin($datatype)
+    { return (NULL) ? (int) NULL : (bool) NULL; }
+
+    protected function intMax($datatype)
+    { return (NULL) ? (int) NULL : (bool) NULL; }
+
+    protected function convertCurrency($amount, $from, $to)
+    { return (NULL) ? (string) NULL : (bool) NULL; }
+}
