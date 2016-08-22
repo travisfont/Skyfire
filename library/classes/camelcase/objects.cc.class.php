98c21c1b0cfa16ac190b2ae4f9be96b0ed1ffc37
Travis van der Font <travis.font@gmail.com>
2016-08-22 17:30:09 +0200

    Camel Case Objects interface for IDE

---
+++ library/classes/camelcase/objects.cc.class.php
@@ -0,0 +1,10 @@
+<?php
+
+// Object interface and function into object inherence
+
+class Objects extends Controller
+{
+    // convertObjectsToArrays
+    protected function convertObjectsToArrays($obj, &$arr)
+    { return (array) NULL; }
+}
