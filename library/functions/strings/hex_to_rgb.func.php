826e2fa2dcf7f5e2440e42c8ee7ab03ea55bc74f
Travis van der Font <travis.font@gmail.com>
2016-08-24 20:26:24 +0200

    refactoring hex_to_rgb()

--- library/functions/strings/hex_to_rgb.func.php
+++ library/functions/strings/hex_to_rgb.func.php
@@ -1,24 +1,30 @@
 <?php 
 
-function hex_to_rgb($hex)
+function hex_to_rgb($hex, $string = FALSE)
 {
-	   $hex = str_replace("#", "", $hex);
-	
-	   if (strlen($hex) == 3)
-       {
-		  $r = hexdec(substr($hex,0,1).substr($hex,0,1));
-		  $g = hexdec(substr($hex,1,1).substr($hex,1,1));
-		  $b = hexdec(substr($hex,2,1).substr($hex,2,1));
-	   }
-       else
-       {
-		  $r = hexdec(substr($hex,0,2));
-		  $g = hexdec(substr($hex,2,2));
-		  $b = hexdec(substr($hex,4,2));
-	   }
+	$hex = str_replace('#', '', $hex);
 
-	   $rgb = array($r, $g, $b);
+	if (strlen($hex) == 3)
+	{
+		$r = hexdec(substr($hex, 0, 1).substr($hex, 0, 1));
+		$g = hexdec(substr($hex, 1, 1).substr($hex, 1, 1));
+		$b = hexdec(substr($hex, 2, 1).substr($hex, 2, 1));
+	}
+	else
+	{
+		$r = hexdec(substr($hex, 0, 2));
+		$g = hexdec(substr($hex, 2, 2));
+		$b = hexdec(substr($hex, 4, 2));
+	}
 
-	   //return implode(",", $rgb); // returns the rgb values separated by commas
-	   return $rgb; // returns an array with the rgb values
+	$rgb = array($r, $g, $b);
+
+	if ($string)
+	{
+	   return (string) implode(",", $rgb); // returns the rgb values separated by commas
+	}
+	else
+	{
+		return (array) $rgb; // returns an array with the rgb values
+	}
 }
