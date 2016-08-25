2783b6404b191a065449357cab8346863496aafd
Travis van der Font <travis.font@gmail.com>
2016-08-25 16:30:58 +0200

    trim_explode() - big fix on $limit $argument

--- library/functions/arrays/trim_explode.func.php
+++ library/functions/arrays/trim_explode.func.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Exokides an string into an array with a trim (after)
+ * explodes an string into an array with a trim (after)
  */
 
-function trim_explode($delimiter, $string, $trim = 'trim', $limit = NULL)
+function trim_explode($delimiter, $string, $trim = 'trim', $limit = PHP_INT_MAX)
 {
     return (array) array_map($trim, explode($delimiter, $string, $limit));
 }
