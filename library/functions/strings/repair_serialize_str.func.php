427760911b411edb0d925cd719008e85aa280273
Travis van der Font <travis.font@gmail.com>
2016-09-13 13:08:34 +0200

    add repair_serialize_str() to string library class

---
+++ library/functions/strings/repair_serialize_str.func.php
@@ -0,0 +1,19 @@
+<?php
+
+//  fix broken serialize string using following function, with multibyte character handling.
+
+function repair_serialize_str($string, $mb_strlen = FALSE)
+{
+    return (string) preg_replace_callback('!s:(\d+):"(.*?)";!', function ($match) use ($mb_strlen)
+    {
+        if ($mb_strlen === TRUE)
+        {
+            return ($match[1] == mb_strlen($match[2])) ? $match[0] : 's:'.mb_strlen($match[2]).':"'.$match[2].'";';
+        }
+        else
+        {
+            return ($match[1] == strlen($match[2])) ? $match[0] : 's:'.strlen($match[2]).':"'.$match[2].'";';
+        }
+    },
+    $string);
+}
