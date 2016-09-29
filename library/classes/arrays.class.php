3ba97f79f93833be6dae085474b3e50371db9288
Travis van der Font <travis.font@gmail.com>
2016-09-29 17:43:09 +0200

    Arrays class - functions alphabetic listing

--- library/classes/arrays.class.php
+++ library/classes/arrays.class.php
@@ -4,156 +4,6 @@
 
 class Arrays extends Controller
 {
-    protected function clear_empty_values(array $array, $keep_zero = TRUE)
-    {
-        //return (array) clear_empty_values($array, $keep_zero);
-        return (array) self::parameters(
-        [
-            'array'     => DT::TYPE_ARRAY,
-            'keep_zero' => DT::BOOL
-        ])
-        ->call(__FUNCTION__)
-        ->with($array, $keep_zero)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    protected function randomize_array(array $arr, $amount = 1)
-    {
-        //return randomize_array($arr, $amount);
-        return (array) self::parameters(
-        [
-            'arr'    => DT::TYPE_ARRAY,
-            'amount' => DT::UINT8
-        ])
-        ->call(__FUNCTION__)
-        ->with($arr, $amount)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    // randomizeArrayElement
-    protected function randomize_array_element(array $array)
-    {
-        //return randomize_array_element($array);
-        return self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::MIXED);
-    }
-
-    // subsortArray
-    protected function subsort_array(array $array, $subkey)
-    {
-        //return subsort_array($array, $subkey);
-        return (array) self::parameters(
-        [
-            'array'  => DT::TYPE_ARRAY,
-            'subkey' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($array, $subkey)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    // convertArrayToObject
-    protected function convert_array_to_object(array $array)
-    {
-        //return convert_array_to_object($array);
-        return self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::MIXED);
-    }
-
-    // inMultiArray
-    protected function in_multi_array($needle, $haystack, $strict = FALSE)
-    {
-        //return in_multi_array($needle, $haystack, $strict);
-        return (bool) self::parameters(
-        [
-            'needle'   => DT::MIXED,
-            'haystack' => DT::TYPE_ARRAY,
-            'strict'   => DT::BOOL
-        ])
-        ->call(__FUNCTION__)
-        ->with($needle, $haystack, $strict)
-        ->returning(DT::BOOL);
-    }
-
-    // isAssociativeArray
-    protected function is_associate_array($array)
-    {
-        //return is_associate_array($array);
-        return (bool) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::BOOL);
-    }
-
-    // trimArrayValues
-    protected function trim_array_values($array)
-    {
-        //return trim_array_values($array);
-        return (array) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    // transposeData
-    protected function transpose_data(array $array)
-    {
-        //return transpose_data($array);
-        return (array) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    // multiExplode
-    protected function multi_explode(array $delimiters, $string)
-    {
-        //return multi_explode($delimiters, $string);
-        return (array) self::parameters(
-        [
-            'delimiters' => DT::TYPE_ARRAY,
-            'string'     => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($delimiters, $string)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    // trimExplode
-    protected function trim_explode($delimiter, $string, $trim = 'trim', $limit = NULL)
-    {
-        //return trim_explode($delimiter, $string, $trim, $limit);
-        return (array) self::parameters(
-        [
-            'delimiters' => DT::TYPE_ARRAY,
-            'string'     => DT::STRING,
-            'trim'       => DT::STRING,
-            'limit'      => DT::MIXED
-        ])
-        ->call(__FUNCTION__)
-        ->with($delimiter, $string, $trim, $limit)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
     protected function array_depth(array $array)
     {
         //return array_depth($array);
@@ -164,18 +14,6 @@ class Arrays extends Controller
         ->call(__FUNCTION__)
         ->with($array)
         ->returning(DT::UINT8);
-    }
-
-    protected function fill_array_key_leading_zeros(array $array)
-    {
-        //return fill_array_key_leading_zeros($array);
-        return (array) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::TYPE_ARRAY);
     }
 
     protected function array_keys_exist(array $needles, array $haystack)
@@ -191,6 +29,82 @@ class Arrays extends Controller
         ->returning(DT::BOOL);
     }
 
+    protected function clear_empty_values(array $array, $keep_zero = TRUE)
+    {
+        //return (array) clear_empty_values($array, $keep_zero);
+        return (array) self::parameters(
+        [
+            'array'     => DT::TYPE_ARRAY,
+            'keep_zero' => DT::BOOL
+        ])
+        ->call(__FUNCTION__)
+        ->with($array, $keep_zero)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function convert_array_to_object(array $array)
+    {
+        //return convert_array_to_object($array);
+        return self::parameters(
+        [
+            'array' => DT::TYPE_ARRAY
+        ])
+        ->call(__FUNCTION__)
+        ->with($array)
+        ->returning(DT::MIXED);
+    }
+
+    protected function fill_array_key_leading_zeros(array $array)
+    {
+        //return fill_array_key_leading_zeros($array);
+        return (array) self::parameters(
+        [
+            'array' => DT::TYPE_ARRAY
+        ])
+        ->call(__FUNCTION__)
+        ->with($array)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function in_multi_array($needle, $haystack, $strict = FALSE)
+    {
+        //return in_multi_array($needle, $haystack, $strict);
+        return (bool) self::parameters(
+        [
+            'needle'   => DT::MIXED,
+            'haystack' => DT::TYPE_ARRAY,
+            'strict'   => DT::BOOL
+        ])
+        ->call(__FUNCTION__)
+        ->with($needle, $haystack, $strict)
+        ->returning(DT::BOOL);
+    }
+
+    protected function is_associate_array($array)
+    {
+        //return is_associate_array($array);
+        return (bool) self::parameters(
+        [
+            'array' => DT::TYPE_ARRAY
+        ])
+        ->call(__FUNCTION__)
+        ->with($array)
+        ->returning(DT::BOOL);
+    }
+
+    protected function multi_explode(array $delimiters, $string)
+    {
+        //return multi_explode($delimiters, $string);
+        return (array) self::parameters(
+        [
+            'delimiters' => DT::TYPE_ARRAY,
+            'string'     => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($delimiters, $string)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
     function preg_array_key_exists($pattern, array $array)
     {
         //return (int) preg_array_key_exists($pattern, $array);
@@ -202,5 +116,82 @@ class Arrays extends Controller
         ->call(__FUNCTION__)
         ->with($pattern, $array)
         ->returning(DT::UINT8);
+    }
+
+    protected function randomize_array(array $arr, $amount = 1)
+    {
+        //return randomize_array($arr, $amount);
+        return (array) self::parameters(
+        [
+            'arr'    => DT::TYPE_ARRAY,
+            'amount' => DT::UINT8
+        ])
+        ->call(__FUNCTION__)
+        ->with($arr, $amount)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function randomize_array_element(array $array)
+    {
+        //return randomize_array_element($array);
+        return self::parameters(
+        [
+            'array' => DT::TYPE_ARRAY
+        ])
+        ->call(__FUNCTION__)
+        ->with($array)
+        ->returning(DT::MIXED);
+    }
+
+    protected function subsort_array(array $array, $subkey)
+    {
+        //return subsort_array($array, $subkey);
+        return (array) self::parameters(
+        [
+            'array'  => DT::TYPE_ARRAY,
+            'subkey' => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($array, $subkey)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function transpose_data(array $array)
+    {
+        //return transpose_data($array);
+        return (array) self::parameters(
+        [
+            'array' => DT::TYPE_ARRAY
+        ])
+        ->call(__FUNCTION__)
+        ->with($array)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function trim_array_values($array)
+    {
+        //return trim_array_values($array);
+        return (array) self::parameters(
+        [
+            'array' => DT::TYPE_ARRAY
+        ])
+        ->call(__FUNCTION__)
+        ->with($array)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function trim_explode($delimiter, $string, $trim = 'trim', $limit = NULL)
+    {
+        //return trim_explode($delimiter, $string, $trim, $limit);
+        return (array) self::parameters(
+        [
+            'delimiters' => DT::TYPE_ARRAY,
+            'string'     => DT::STRING,
+            'trim'       => DT::STRING,
+            'limit'      => DT::MIXED
+        ])
+        ->call(__FUNCTION__)
+        ->with($delimiter, $string, $trim, $limit)
+        ->returning(DT::TYPE_ARRAY);
     }
 }
--- library/classes/camelcase/arrays.cc.class.php
+++ library/classes/camelcase/arrays.cc.class.php
@@ -4,8 +4,32 @@
 
 class Arrays extends Controller
 {
+    protected function arrayDepth(array $array)
+    { return (int) NULL; }
+
+    protected function arrayKeysExist(array $needles, array $haystack)
+    { return (bool) NULL; }
+
     protected function clearEmptyValues(array $array, $keep_zero = TRUE)
     { return (array) NULL; }
+
+    protected function convertArrayToObject(array $array)
+    { return var_export(NULL); }
+
+    protected function fillArrayKeyLeadingZeros(array $array)
+    { return (array) NULL; }
+
+    protected function inMultiArray($needle, $haystack, $strict = FALSE)
+    { return (bool) NULL; }
+
+    protected function isAssociativeArray($array)
+    { return (bool) NULL; }
+
+    protected function multiExplode(array $delimiters, $string)
+    { return (array) NULL; }
+
+    function pregArrayKeyExists($pattern, array $array)
+    { return (int) NULL; }
 
     protected function randomizeArray(array $arr, $amount = 1)
     { return (array) NULL; }
@@ -16,36 +40,12 @@ class Arrays extends Controller
     protected function subsortArray(array $array, $subkey)
     { return (array) NULL; }
 
-    protected function convertArrayToObject(array $array)
-    { return var_export(NULL); }
-
-    protected function inMultiArray($needle, $haystack, $strict = FALSE)
-    { return (bool) NULL; }
-
-    protected function isAssociativeArray($array)
-    { return (bool) NULL; }
+    protected function transposeData(array $array)
+    { return (array) NULL; }
 
     protected function trimArrayValues($array)
     { return (array) NULL; }
 
-    protected function transposeData(array $array)
-    { return (array) NULL; }
-
-    protected function multiExplode(array $delimiters, $string)
-    { return (array) NULL; }
-
     protected function trimExplode($delimiter, $string, $trim = 'trim', $limit = NULL)
     { return (array) NULL; }
-
-    protected function arrayDepth(array $array)
-    { return (int) NULL; }
-
-    protected function fillArrayKeyLeadingZeros(array $array)
-    { return (array) NULL; }
-
-    protected function arrayKeysExist(array $needles, array $haystack)
-    { return (bool) NULL; }
-
-    function pregArrayKeyExists($pattern, array $array)
-    { return (int) NULL; }
 }
