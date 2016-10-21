7f1e297e1b5d7f13fdde6a5718e4a1cac092cd40
Travis van der Font <travis.font@gmail.com>
2016-10-21 19:30:24 +0200

    HTML class - functions alphabetic listing

--- library/classes/camelcase/html.cc.class.php
+++ library/classes/camelcase/html.cc.class.php
@@ -4,8 +4,45 @@
 
 class HTML extends Controller
 {
+    protected function appendMissingTag() {}
+
+    protected function compressCss($buffer)
+    { return (string) NULL; }
+
+    protected function compressHtml($content)
+    { return (string) NULL; }
+
+    protected function countHtmlOutputLines() {}
+    protected function createDivTable() {}
+
+    protected function getElementAllAttributes($tag_name, $html, $encoding = 'UTF-8')
+    { return (array) NULL; }
+
+    protected function getPageTitle() {}
+
+    protected function isInput($html, $encoding = 'UTF-8')
+    { return (bool) NULL; }
+
+    protected function isSelect($html, $encoding = 'UTF-8')
+    { return (bool) NULL; }
+
     protected function leftTrimBrTag($string)
     { return (string) NULL; }
+
+    protected function minifyCss($css)
+    { return (string) NULL; }
+
+    protected function minifyHtml($content)
+    { return (string) NULL; }
+
+    protected function minifyJs($javascript)
+    { return (string) NULL; }
+
+    protected function parseSelectorValues($selector_html, $encoding = 'UTF-8')
+    { return (array) NULL; }
+
+    protected function parseSelectorValuesByName($name, $selector_html, $encoding = 'UTF-8')
+    { return (array) NULL; }
 
     protected function replaceHtmlTag($string, $tag, $replace = '')
     { return (string) NULL; }
@@ -16,7 +53,10 @@ class HTML extends Controller
     protected function replacePToBr($string)
     { return (string) NULL; }
 
-    protected function truncateHtml($text, $length = 100, $ending = '...', $exact = FALSE, $consider_html = TRUE)
+    protected function safeTruncate($input, $break = ' ', $end_text = '...', $limit = 255, $tidy_html = TRUE, $strip_html = FALSE)
+    { return (string) NULL; }
+
+    protected function stripHtmlTable($string)
     { return (string) NULL; }
 
     protected function stripHtmlTag($string, $tag)
@@ -25,39 +65,6 @@ class HTML extends Controller
     protected function stripHtmlTagStyles($string)
     { return (string) NULL; }
 
-    protected function stripHtmlTable($string)
-    { return (string) NULL; }
-
-    protected function compressHtml($content)
-    { return (string) NULL; }
-
-    protected function minifyHtml($content)
-    { return (string) NULL; }
-
-    protected function minifyCss($css)
-    { return (string) NULL; }
-
-    protected function minifyJs($javascript)
-    { return (string) NULL; }
-
-    protected function isInput($html, $encoding = 'UTF-8')
-    { return (bool) NULL; }
-
-    protected function isSelect($html, $encoding = 'UTF-8')
-    { return (bool) NULL; }
-
-    protected function getElementAllAttributes($tag_name, $html, $encoding = 'UTF-8')
-    { return (array) NULL; }
-
-    protected function parseSelectorValuesByName($name, $selector_html, $encoding = 'UTF-8')
-    { return (array) NULL; }
-
-    protected function parseSelectorValues($selector_html, $encoding = 'UTF-8')
-    { return (array) NULL; }
-
-    protected function safeTruncate($input, $break = ' ', $end_text = '...', $limit = 255, $tidy_html = TRUE, $strip_html = FALSE)
-    { return (string) NULL; }
-
-    protected function compressCss($buffer)
+    protected function truncateHtml($text, $length = 100, $ending = '...', $exact = FALSE, $consider_html = TRUE)
     { return (string) NULL; }
 }
--- library/classes/html.class.php
+++ library/classes/html.class.php
@@ -4,6 +4,89 @@
 
 class HTML extends Controller
 {
+    protected function append_missing_tag() {}
+
+    protected function br2nl($input)
+    {
+        //return (string) br2nl($input);
+        return (string) self::parameters(
+        [
+            'input' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($input)
+        ->returning(DT::TEXT);
+    }
+
+    protected function compress_css($buffer)
+    {
+        //return (string) compress_css($buffer);
+        return (string) self::parameters(
+        [
+            'buffer' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($buffer)
+        ->returning(DT::TEXT);
+    }
+
+    protected function compress_html($content)
+    {
+        //return (string) compress_html($content);
+        return (string) self::parameters(
+        [
+            'content' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($content)
+        ->returning(DT::TEXT);
+    }
+
+    protected function count_html_output_lines() {}
+    protected function create_div_table() {}
+
+    protected function get_element_all_attributes($tag_name, $html, $encoding = 'UTF-8')
+    {
+        //return (array) get_element_all_attributes($tag_name, $html, $encoding);
+        return (array) self::parameters(
+        [
+            'tag_name' => DT::STRING,
+            'html'     => DT::TEXT,
+            'encoding' => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($tag_name, $html, $encoding)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function get_page_title() {}
+
+    protected function is_input($html, $encoding = 'UTF-8')
+    {
+        //return (bool) is_input($html, $encoding);
+        return (bool) self::parameters(
+        [
+            'html'     => DT::TEXT,
+            'encoding' => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($html, $encoding)
+        ->returning(DT::BOOL);
+    }
+
+    protected function is_select($html, $encoding = 'UTF-8')
+    {
+        //return (bool) is_select($html, $encoding);
+        return (bool) self::parameters(
+        [
+            'html'     => DT::TEXT,
+            'encoding' => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($html, $encoding)
+        ->returning(DT::BOOL);
+    }
+
     protected function left_trim_br_tag($string)
     {
         //return left_trim_br_tag($string);
@@ -14,6 +97,69 @@ class HTML extends Controller
         ->call(__FUNCTION__)
         ->with($string)
         ->returning(DT::TEXT);
+    }
+
+    protected function minify_css($css)
+    {
+        //return (string) minify_css($css);
+        return (string) self::parameters(
+        [
+            'css' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($css)
+        ->returning(DT::TEXT);
+    }
+
+    protected function minify_html($content)
+    {
+        //return (string) minify_html($content);
+        return (string) self::parameters(
+        [
+            'content' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($content)
+        ->returning(DT::TEXT);
+    }
+
+    protected function minify_js($javascript)
+    {
+        //return (string) minify_js($javascript);
+        return (string) self::parameters(
+        [
+            'javascript' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($javascript)
+        ->returning(DT::TEXT);
+    }
+
+    protected function parse_selector_values($selector_html, $encoding = 'UTF-8')
+    {
+        //return (array) parse_selector_values($selector_html, $encoding);
+        return (array) self::parameters(
+        [
+            'selector_html' => DT::TEXT,
+            'encoding'      => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($selector_html, $encoding)
+        ->returning(DT::TYPE_ARRAY);
+    }
+
+    protected function parse_selector_values_by_name($name, $selector_html, $encoding = 'UTF-8')
+    {
+        //return (array) parse_selector_values_by_name($name, $selector_html, $encoding);
+        return (array) self::parameters(
+        [
+            'name'          => DT::STRING,
+            'selector_html' => DT::TEXT,
+            'encoding'      => DT::STRING
+        ])
+        ->call(__FUNCTION__)
+        ->with($name, $selector_html, $encoding)
+        ->returning(DT::TYPE_ARRAY);
     }
 
     protected function replace_html_tag($string, $tag, $replace = '')
@@ -54,19 +200,32 @@ class HTML extends Controller
         ->returning(DT::TEXT);
     }
 
-    protected function truncate_html($text, $length = 100, $ending = '...', $exact = FALSE, $consider_html = TRUE)
+    protected function safe_truncate($input, $break = ' ', $end_text = '...', $limit = 255, $tidy_html = TRUE, $strip_html = FALSE)
     {
-        //return (string) truncate_html($text, $length, $ending, $exact, $consider_html);
+        //return (string) safe_truncate($input, $break, $end_text, $limit, $tidy_html, $strip_html);
         return (string) self::parameters(
         [
-            'text'          => DT::TEXT,
-            'length'        => DT::UINT32,
-            'ending'        => DT::STRING,
-            'exact'         => DT::BOOL,
-            'consider_html' => DT::BOOL
+            'input'      => DT::TEXT,
+            'break'      => DT::STRING,
+            'end_text'   => DT::STRING,
+            'limit'      => DT::UINT32,
+            'tidy_html'  => DT::BOOL,
+            'strip_html' => DT::BOOL
         ])
         ->call(__FUNCTION__)
-        ->with($text, $length, $ending, $exact, $consider_html)
+        ->with($input, $break, $end_text, $limit, $tidy_html, $strip_html)
+        ->returning(DT::TEXT);
+    }
+
+    protected function strip_html_table($string)
+    {
+        //return (string) strip_html_table($string);
+        return (string) self::parameters(
+        [
+            'string' => DT::TEXT
+        ])
+        ->call(__FUNCTION__)
+        ->with($string)
         ->returning(DT::TEXT);
     }
 
@@ -95,171 +254,19 @@ class HTML extends Controller
         ->returning(DT::TEXT);
     }
 
-    protected function strip_html_table($string)
+    protected function truncate_html($text, $length = 100, $ending = '...', $exact = FALSE, $consider_html = TRUE)
     {
-        //return (string) strip_html_table($string);
+        //return (string) truncate_html($text, $length, $ending, $exact, $consider_html);
         return (string) self::parameters(
         [
-            'string' => DT::TEXT
+            'text'          => DT::TEXT,
+            'length'        => DT::UINT32,
+            'ending'        => DT::STRING,
+            'exact'         => DT::BOOL,
+            'consider_html' => DT::BOOL
         ])
         ->call(__FUNCTION__)
-        ->with($string)
-        ->returning(DT::TEXT);
-    }
-
-    protected function compress_html($content)
-    {
-        //return (string) compress_html($content);
-        return (string) self::parameters(
-        [
-            'content' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($content)
-        ->returning(DT::TEXT);
-    }
-
-    protected function minify_html($content)
-    {
-        //return (string) minify_html($content);
-        return (string) self::parameters(
-        [
-            'content' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($content)
-        ->returning(DT::TEXT);
-    }
-
-    protected function minify_css($css)
-    {
-        //return (string) minify_css($css);
-        return (string) self::parameters(
-        [
-            'css' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($css)
-        ->returning(DT::TEXT);
-    }
-
-    protected function minify_js($javascript)
-    {
-        //return (string) minify_js($javascript);
-        return (string) self::parameters(
-        [
-            'javascript' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($javascript)
-        ->returning(DT::TEXT);
-    }
-
-    protected function is_input($html, $encoding = 'UTF-8')
-    {
-        //return (bool) is_input($html, $encoding);
-        return (bool) self::parameters(
-        [
-            'html'     => DT::TEXT,
-            'encoding' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($html, $encoding)
-        ->returning(DT::BOOL);
-    }
-
-    protected function is_select($html, $encoding = 'UTF-8')
-    {
-        //return (bool) is_select($html, $encoding);
-        return (bool) self::parameters(
-        [
-            'html'     => DT::TEXT,
-            'encoding' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($html, $encoding)
-        ->returning(DT::BOOL);
-    }
-
-    protected function get_element_all_attributes($tag_name, $html, $encoding = 'UTF-8')
-    {
-        //return (array) get_element_all_attributes($tag_name, $html, $encoding);
-        return (array) self::parameters(
-        [
-            'tag_name' => DT::STRING,
-            'html'     => DT::TEXT,
-            'encoding' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($tag_name, $html, $encoding)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    protected function parse_selector_values_by_name($name, $selector_html, $encoding = 'UTF-8')
-    {
-        //return (array) parse_selector_values_by_name($name, $selector_html, $encoding);
-        return (array) self::parameters(
-        [
-            'name'          => DT::STRING,
-            'selector_html' => DT::TEXT,
-            'encoding'      => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($name, $selector_html, $encoding)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    protected function parse_selector_values($selector_html, $encoding = 'UTF-8')
-    {
-        //return (array) parse_selector_values($selector_html, $encoding);
-        return (array) self::parameters(
-        [
-            'selector_html' => DT::TEXT,
-            'encoding'      => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($selector_html, $encoding)
-        ->returning(DT::TYPE_ARRAY);
-    }
-
-    protected function safe_truncate($input, $break = ' ', $end_text = '...', $limit = 255, $tidy_html = TRUE, $strip_html = FALSE)
-    {
-        //return (string) safe_truncate($input, $break, $end_text, $limit, $tidy_html, $strip_html);
-        return (string) self::parameters(
-        [
-            'input'      => DT::TEXT,
-            'break'      => DT::STRING,
-            'end_text'   => DT::STRING,
-            'limit'      => DT::UINT32,
-            'tidy_html'  => DT::BOOL,
-            'strip_html' => DT::BOOL
-        ])
-        ->call(__FUNCTION__)
-        ->with($input, $break, $end_text, $limit, $tidy_html, $strip_html)
-        ->returning(DT::TEXT);
-    }
-
-    protected function compress_css($buffer)
-    {
-        //return (string) compress_css($buffer);
-        return (string) self::parameters(
-        [
-            'buffer' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($buffer)
-        ->returning(DT::TEXT);
-    }
-
-    protected function br2nl($input)
-    {
-        //return (string) br2nl($input);
-        return (string) self::parameters(
-        [
-            'input' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($input)
+        ->with($text, $length, $ending, $exact, $consider_html)
         ->returning(DT::TEXT);
     }
 }
