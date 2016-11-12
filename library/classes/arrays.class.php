67f27d76b47e0cfef2d30e7b5483b7c9e941720e
Travis van der Font <travis.font@gmail.com>
2016-11-12 14:53:29 +0100

    Arrays Library - added strict type option

--- library/classes/arrays.class.php
+++ library/classes/arrays.class.php
@@ -6,192 +6,282 @@ class Arrays extends Controller
 {
     protected function array_depth(array $array)
     {
-        //return array_depth($array);
-        return (int) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::UINT8);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (int) self::parameters(
+            [
+                'array' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($array)
+            ->returning(DT::UINT8);
+        }
+        else
+        {
+            return (int) array_depth($array);
+        }
     }
 
     protected function array_keys_exist(array $needles, array $haystack)
     {
-        //return (bool) array_keys_exist($needles, $haystack);
-        return (bool) self::parameters(
-        [
-            'needles'  => DT::TYPE_ARRAY,
-            'haystack' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($needles, $haystack)
-        ->returning(DT::BOOL);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (bool) self::parameters(
+            [
+                'needles'  => DT::TYPE_ARRAY,
+                'haystack' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($needles, $haystack)
+            ->returning(DT::BOOL);
+        }
+        else
+        {
+            return (bool) array_keys_exist($needles, $haystack);
+        }
     }
 
     protected function clear_empty_values(array $array, $keep_zero = TRUE)
     {
-        //return (array) clear_empty_values($array, $keep_zero);
-        return (array) self::parameters(
-        [
-            'array'     => DT::TYPE_ARRAY,
-            'keep_zero' => DT::BOOL
-        ])
-        ->call(__FUNCTION__)
-        ->with($array, $keep_zero)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'array'     => DT::TYPE_ARRAY,
+                'keep_zero' => DT::BOOL
+            ])
+            ->call(__FUNCTION__)
+            ->with($array, $keep_zero)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) clear_empty_values($array, $keep_zero);
+        }
     }
 
     protected function convert_array_to_object(array $array)
     {
-        //return convert_array_to_object($array);
-        return self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::MIXED);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return self::parameters(
+            [
+                'array' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($array)
+            ->returning(DT::MIXED);
+        }
+        else
+        {
+            return convert_array_to_object($array);
+        }
     }
 
     protected function fill_array_key_leading_zeros(array $array)
     {
-        //return fill_array_key_leading_zeros($array);
-        return (array) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'array' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($array)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) fill_array_key_leading_zeros($array);
+        }
     }
 
     protected function in_multi_array($needle, $haystack, $strict = FALSE)
     {
-        //return in_multi_array($needle, $haystack, $strict);
-        return (bool) self::parameters(
-        [
-            'needle'   => DT::MIXED,
-            'haystack' => DT::TYPE_ARRAY,
-            'strict'   => DT::BOOL
-        ])
-        ->call(__FUNCTION__)
-        ->with($needle, $haystack, $strict)
-        ->returning(DT::BOOL);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (bool) self::parameters(
+            [
+                'needle'   => DT::MIXED,
+                'haystack' => DT::TYPE_ARRAY,
+                'strict'   => DT::BOOL
+            ])
+            ->call(__FUNCTION__)
+            ->with($needle, $haystack, $strict)
+            ->returning(DT::BOOL);
+        }
+        else
+        {
+            return (bool) in_multi_array($needle, $haystack, $strict);
+        }
     }
 
     protected function is_associate_array($array)
     {
-        //return is_associate_array($array);
-        return (bool) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::BOOL);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (bool) self::parameters(
+            [
+                'array' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($array)
+            ->returning(DT::BOOL);
+        }
+        else
+        {
+            return (bool) is_associate_array($array);
+        }
     }
 
     protected function multi_explode(array $delimiters, $string)
     {
-        //return multi_explode($delimiters, $string);
-        return (array) self::parameters(
-        [
-            'delimiters' => DT::TYPE_ARRAY,
-            'string'     => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($delimiters, $string)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'delimiters' => DT::TYPE_ARRAY,
+                'string'     => DT::STRING
+            ])
+            ->call(__FUNCTION__)
+            ->with($delimiters, $string)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) multi_explode($delimiters, $string);
+        }
     }
 
     function preg_array_key_exists($pattern, array $array)
     {
-        //return (int) preg_array_key_exists($pattern, $array);
-        return (int) self::parameters(
-        [
-            'pattern' => DT::STRING,
-            'array'   => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($pattern, $array)
-        ->returning(DT::UINT8);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (int) self::parameters(
+            [
+                'pattern' => DT::STRING,
+                'array'   => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($pattern, $array)
+            ->returning(DT::UINT8);
+        }
+        else
+        {
+            return (int) preg_array_key_exists($pattern, $array);
+        }
     }
 
     protected function randomize_array(array $arr, $amount = 1)
     {
-        //return randomize_array($arr, $amount);
-        return (array) self::parameters(
-        [
-            'arr'    => DT::TYPE_ARRAY,
-            'amount' => DT::UINT8
-        ])
-        ->call(__FUNCTION__)
-        ->with($arr, $amount)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'arr'    => DT::TYPE_ARRAY,
+                'amount' => DT::UINT8
+            ])
+            ->call(__FUNCTION__)
+            ->with($arr, $amount)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) randomize_array($arr, $amount);
+        }
     }
 
     protected function randomize_array_element(array $array)
     {
-        //return randomize_array_element($array);
-        return self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::MIXED);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return self::parameters(
+            [
+                'array' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($array)
+            ->returning(DT::MIXED);
+        }
+        else
+        {
+            return randomize_array_element($array);
+        }
     }
 
     protected function subsort_array(array $array, $subkey)
     {
-        //return subsort_array($array, $subkey);
-        return (array) self::parameters(
-        [
-            'array'  => DT::TYPE_ARRAY,
-            'subkey' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($array, $subkey)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'array'  => DT::TYPE_ARRAY,
+                'subkey' => DT::STRING
+            ])
+            ->call(__FUNCTION__)
+            ->with($array, $subkey)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) subsort_array($array, $subkey);
+        }
     }
 
     protected function transpose_data(array $array)
     {
-        //return transpose_data($array);
-        return (array) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'array' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($array)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) transpose_data($array);
+        }
     }
 
     protected function trim_array_values($array)
     {
-        //return trim_array_values($array);
-        return (array) self::parameters(
-        [
-            'array' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($array)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'array' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($array)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) trim_array_values($array);
+        }
     }
 
     protected function trim_explode($delimiter, $string, $trim = 'trim', $limit = NULL)
     {
-        //return trim_explode($delimiter, $string, $trim, $limit);
-        return (array) self::parameters(
-        [
-            'delimiters' => DT::TYPE_ARRAY,
-            'string'     => DT::STRING,
-            'trim'       => DT::STRING,
-            'limit'      => DT::MIXED
-        ])
-        ->call(__FUNCTION__)
-        ->with($delimiter, $string, $trim, $limit)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'delimiters' => DT::TYPE_ARRAY,
+                'string'     => DT::STRING,
+                'trim'       => DT::STRING,
+                'limit'      => DT::MIXED
+            ])
+            ->call(__FUNCTION__)
+            ->with($delimiter, $string, $trim, $limit)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) trim_explode($delimiter, $string, $trim, $limit);
+        }
     }
 }
