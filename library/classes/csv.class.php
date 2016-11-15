c365a155d6fd832423b290123c0e0f55023ed264
Travis van der Font <travis.font@gmail.com>
2016-11-15 22:26:31 +0100

    CSV Library - added strict type option

--- library/classes/csv.class.php
+++ library/classes/csv.class.php
@@ -10,50 +10,74 @@ class Csv extends Controller
      */
     protected function array_getcsv($data)
     {
-        //return array_getcsv($data);
-        return (array) self::parameters(
-        [
-            'data' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($data)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'data' => DT::STRING
+            ])
+            ->call(__FUNCTION__)
+            ->with($data)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) array_getcsv($data);
+        }
     }
 
     protected function cvs_file_to_assoc_array($filename, $delimiter = ',')
     {
-        //return cvs_file_to_assoc_array($filename, $delimiter);
-        return self::parameters(
-        [
-            'filename'  => DT::STRING,
-            'delimiter' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($filename, $delimiter)
-        ->returning([DT::TYPE_ARRAY, DT::BOOL]);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return self::parameters(
+            [
+                'filename'  => DT::STRING,
+                'delimiter' => DT::STRING
+            ])
+            ->call(__FUNCTION__)
+            ->with($filename, $delimiter)
+            ->returning([DT::TYPE_ARRAY, DT::BOOL]);
+        }
+        else
+        {
+            return cvs_file_to_assoc_array($filename, $delimiter);
+        }
     }
 
     protected function csv_to_array($data)
     {
-        //return csv_to_array($data);
-        return (array) self::parameters(
-        [
-            'data' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($data)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'data' => DT::TEXT
+            ])
+            ->call(__FUNCTION__)
+            ->with($data)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return csv_to_array($data);
+        }
     }
 
     protected function csv_to_assoc_array($data)
     {
-        //return csv_to_assoc_array($data);
-        return (array) self::parameters(
-        [
-            'data' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($data)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'data' => DT::TEXT
+            ])
+            ->call(__FUNCTION__)
+            ->with($data)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) csv_to_assoc_array($data);
+        }
     }
 }
