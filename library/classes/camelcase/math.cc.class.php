55f3f41651b8d13123c70a0811adafd9a09394db
Travis van der Font <travis.font@gmail.com>
2016-11-18 23:25:19 +0100

    Math class - functions alphabetic listing

--- library/classes/camelcase/math.cc.class.php
+++ library/classes/camelcase/math.cc.class.php
@@ -4,41 +4,26 @@
 
 class Math extends Controller
 {
-    protected function luhn($number)
-    { return (bool) NULL; }
-
-    protected function randomizeCrypto($min, $max)
-    { return (int) NULL; }
-
     protected function calculateAge($date, $format = 'm/d/Y')
     { return (float) NULL; }
 
     protected function calculateDistance($lat1, $lng1, $lat2, $lng2, $miles = FALSE)
     { return (float) NULL; }
 
-    protected function roundDollar($amount)
-    { return (float) NULL; }
+    protected function convertCurrency($amount, $from, $to)
+    { return (NULL) ? (string) NULL : (bool) NULL; }
 
     protected function formatCurrency($floatcurr, $curr = 'USD')
     { return (string) NULL; }
 
-    protected function randomNumber($digits, $leading_zeros = FALSE)
-    { return (NULL) ? (int) NULL : (string) NULL; }
-
     protected function getPercentageOf($percentage, $number)
     { return (float) NULL; }
 
-    protected function leadingZero($num, $places = 0)
-    { return (string) NULL; }
+    protected function intMax($datatype)
+    { return (NULL) ? (int) NULL : (bool) NULL; }
 
-    protected function roundMins($hour, $minutes = 1, $format = 'H:i')
-    { return (NULL) ? (bool) NULL : (string) NULL; }
-
-    protected function savedPercentage($original_price, $current_price)
-    { return (string) NULL; }
-
-    protected function absint($number)
-    { return (int) NULL; }
+    protected function intMin($datatype)
+    { return (NULL) ? (int) NULL : (bool) NULL; }
 
     protected function isEven($number)
     { return (float) NULL; }
@@ -46,12 +31,29 @@ class Math extends Controller
     protected function isOdd($number)
     { return (float) NULL; }
 
-    protected function intMin($datatype)
-    { return (NULL) ? (int) NULL : (bool) NULL; }
+    protected function leadingZero($num, $places = 0)
+    { return (string) NULL; }
 
-    protected function intMax($datatype)
-    { return (NULL) ? (int) NULL : (bool) NULL; }
+    protected function luhn($number)
+    { return (bool) NULL; }
 
-    protected function convertCurrency($amount, $from, $to)
-    { return (NULL) ? (string) NULL : (bool) NULL; }
+    protected function randomNumber($digits, $leading_zeros = FALSE)
+    { return (NULL) ? (int) NULL : (string) NULL; }
+
+    protected function randomizeCrypto($min, $max)
+    { return (int) NULL; }
+
+    protected function roundDollar($amount)
+    { return (float) NULL; }
+
+    protected function roundMins($hour, $minutes = 1, $format = 'H:i')
+    { return (NULL) ? (bool) NULL : (string) NULL; }
+
+    protected function savedPercentage($original_price, $current_price)
+    { return (string) NULL; }
+
+    // valid_creditcard
+    // generate_creditcard_test_number
+    // get_percentage
+    // is_creditcard
 }
--- library/classes/math.class.php
+++ library/classes/math.class.php
@@ -4,40 +4,21 @@
 
 class Math extends Controller
 {
-    protected function luhn($number)
-    {
-        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
-        {
-            return (bool) self::parameters(
-            [
-                'number' => [DT::INT64, DT::UINT64]
-            ])
-            ->call(__FUNCTION__)
-            ->with($number)
-            ->returning(DT::BOOL);
-        }
-        else
-        {
-            return (bool) luhn($number);
-        }
-    }
-
-    protected function randomize_crypto($min, $max)
+    protected function absint($number)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
         {
             return (int) self::parameters(
             [
-                'min' => DT::UINT64,
-                'max' => DT::UINT64
+                'number' => DT::NUMBER
             ])
             ->call(__FUNCTION__)
-            ->with($min, $max)
+            ->with($number)
             ->returning(DT::UINT64);
         }
         else
         {
-            return (int) randomize_crypto($min, $max);
+            return (int) absint($number);
         }
     }
 
@@ -82,21 +63,23 @@ class Math extends Controller
         }
     }
 
-    protected function round_dollar($amount)
+    protected function convert_currency($amount, $from, $to)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
         {
-            return (float) self::parameters(
+            return self::parameters(
             [
-                'amount' => [DT::INT64, DT::UINT64, DT::FLOAT]
+                'amount' => [DT::INT64,DT::FLOAT],
+                'from'   =>  DT::STRING,
+                'to'     =>  DT::STRING
             ])
             ->call(__FUNCTION__)
-            ->with($amount)
-            ->returning(DT::FLOAT);
+            ->with($amount, $from, $to)
+            ->returning([DT::STRING, DT::BOOL]);
         }
         else
         {
-            return (float) round_dollar($amount);
+            return convert_currency($amount, $from, $to);
         }
     }
 
@@ -119,25 +102,6 @@ class Math extends Controller
         }
     }
 
-    protected function random_number($digits, $leading_zeros = FALSE)
-    {
-        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
-        {
-            return self::parameters(
-            [
-                'digits'        => DT::UINT64,
-                'leading_zeros' => DT::BOOL
-            ])
-            ->call(__FUNCTION__)
-            ->with($digits, $leading_zeros)
-            ->returning([DT::UINT64, DT::STRING]);
-        }
-        else
-        {
-            return random_number($digits, $leading_zeros);
-        }
-    }
-
     protected function get_percentage_of($percentage, $number)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
@@ -157,79 +121,39 @@ class Math extends Controller
         }
     }
 
-    protected function leading_zero($num, $places = 0)
-    {
-        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
-        {
-            return (string) self::parameters(
-            [
-                'num'    => [DT::INT64, DT::UINT64],
-                'places' =>  DT::UINT8
-            ])
-            ->call(__FUNCTION__)
-            ->with($num, $places)
-            ->returning(DT::STRING);
-        }
-        else
-        {
-            return (string) leading_zero($num, $places);
-        }
-    }
-
-    protected function round_mins($hour, $minutes = 1, $format = "H:i")
+    protected function int_max($datatype)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
         {
             return self::parameters(
             [
-                'hour'    => DT::STRING,
-                'minutes' => DT::UINT8,
-                'format'  => DT::STRING
+                'datatype' => DT::STRING
             ])
             ->call(__FUNCTION__)
-            ->with($hour, $minutes, $format)
-            ->returning([DT::BOOL, DT::STRING]);
+            ->with($datatype)
+            ->returning([DT::UINT64, DT::BOOL]);
         }
         else
         {
-            return round_mins($hour, $minutes, $format);
+            return int_max($datatype);
         }
     }
 
-    protected function saved_percentage($original_price, $current_price)
+    protected function int_min($datatype)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
         {
-            return (string) self::parameters(
+            return self::parameters(
             [
-                'original_price' => [DT::INT64, DT::FLOAT],
-                'current_price'  => [DT::INT64, DT::FLOAT]
+                'datatype' => DT::STRING
             ])
             ->call(__FUNCTION__)
-            ->with($original_price, $current_price)
-            ->returning(DT::STRING);
+            ->with($datatype)
+            ->returning([DT::INT64, DT::BOOL]);
         }
         else
         {
-            return (string) saved_percentage($original_price, $current_price);
-        }
-    }
-
-    protected function absint($number)
-    {
-        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
-        {
-            return (int) self::parameters(
-            [
-                'number' => DT::NUMBER
-            ])
-            ->call(__FUNCTION__)
-            ->with($number)
-            ->returning(DT::UINT64);
-        }
-        else
-        {
-            return (int) absint($number);
+            return int_min($datatype);
         }
     }
 
@@ -269,59 +193,135 @@ class Math extends Controller
         }
     }
 
-    protected function int_min($datatype)
+    protected function leading_zero($num, $places = 0)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
         {
-            return self::parameters(
+            return (string) self::parameters(
             [
-                'datatype' => DT::STRING
+                'num'    => [DT::INT64, DT::UINT64],
+                'places' =>  DT::UINT8
             ])
             ->call(__FUNCTION__)
-            ->with($datatype)
-            ->returning([DT::INT64, DT::BOOL]);
+            ->with($num, $places)
+            ->returning(DT::STRING);
         }
         else
         {
-            return int_min($datatype);
+            return (string) leading_zero($num, $places);
         }
     }
 
-    protected function int_max($datatype)
+    protected function luhn($number)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
         {
-            return self::parameters(
+            return (bool) self::parameters(
             [
-                'datatype' => DT::STRING
+                'number' => [DT::INT64, DT::UINT64]
             ])
             ->call(__FUNCTION__)
-            ->with($datatype)
-            ->returning([DT::UINT64, DT::BOOL]);
+            ->with($number)
+            ->returning(DT::BOOL);
         }
         else
         {
-            return int_max($datatype);
+            return (bool) luhn($number);
         }
     }
 
-    protected function convert_currency($amount, $from, $to)
+    protected function random_number($digits, $leading_zeros = FALSE)
     {
         if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
         {
             return self::parameters(
             [
-                'amount' => [DT::INT64,DT::FLOAT],
-                'from'   =>  DT::STRING,
-                'to'     =>  DT::STRING
+                'digits'        => DT::UINT64,
+                'leading_zeros' => DT::BOOL
             ])
             ->call(__FUNCTION__)
-            ->with($amount, $from, $to)
-            ->returning([DT::STRING, DT::BOOL]);
+            ->with($digits, $leading_zeros)
+            ->returning([DT::UINT64, DT::STRING]);
         }
         else
         {
-            return convert_currency($amount, $from, $to);
+            return random_number($digits, $leading_zeros);
+        }
+    }
+
+    protected function randomize_crypto($min, $max)
+    {
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (int) self::parameters(
+            [
+                'min' => DT::UINT64,
+                'max' => DT::UINT64
+            ])
+            ->call(__FUNCTION__)
+            ->with($min, $max)
+            ->returning(DT::UINT64);
+        }
+        else
+        {
+            return (int) randomize_crypto($min, $max);
+        }
+    }
+
+    protected function round_dollar($amount)
+    {
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (float) self::parameters(
+            [
+                'amount' => [DT::INT64, DT::UINT64, DT::FLOAT]
+            ])
+            ->call(__FUNCTION__)
+            ->with($amount)
+            ->returning(DT::FLOAT);
+        }
+        else
+        {
+            return (float) round_dollar($amount);
+        }
+    }
+
+    protected function round_mins($hour, $minutes = 1, $format = "H:i")
+    {
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return self::parameters(
+            [
+                'hour'    => DT::STRING,
+                'minutes' => DT::UINT8,
+                'format'  => DT::STRING
+            ])
+            ->call(__FUNCTION__)
+            ->with($hour, $minutes, $format)
+            ->returning([DT::BOOL, DT::STRING]);
+        }
+        else
+        {
+            return round_mins($hour, $minutes, $format);
+        }
+    }
+
+    protected function saved_percentage($original_price, $current_price)
+    {
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (string) self::parameters(
+            [
+                'original_price' => [DT::INT64, DT::FLOAT],
+                'current_price'  => [DT::INT64, DT::FLOAT]
+            ])
+            ->call(__FUNCTION__)
+            ->with($original_price, $current_price)
+            ->returning(DT::STRING);
+        }
+        else
+        {
+            return (string) saved_percentage($original_price, $current_price);
         }
     }
 }
