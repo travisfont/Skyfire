925a6e24105f2f05c97281df21cca4d945a39579
Travis van der Font <travis.font@gmail.com>
2016-11-25 21:05:21 +0100

    XML Library - added strict type option

--- library/classes/xml.class.php
+++ library/classes/xml.class.php
@@ -6,74 +6,110 @@ class XML extends Controller
 {
     protected function format_xml_string($xml)
     {
-        //return format_xml_string($xml);
-        return (string) self::parameters(
-        [
-            'xml' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($xml)
-        ->returning(DT::TEXT);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (string) self::parameters(
+            [
+                'xml' => DT::TEXT
+            ])
+            ->call(__FUNCTION__)
+            ->with($xml)
+            ->returning(DT::TEXT);
+        }
+        else
+        {
+            return (string) format_xml_string($xml);
+        }
     }
 
     protected function is_atom($feedxml)
     {
-        //return is_atom($feedxml);
-        return (string) self::parameters(
-        [
-            'feedxml' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($feedxml)
-        ->returning(DT::TEXT);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (bool) self::parameters(
+            [
+                'feedxml' => DT::TEXT
+            ])
+            ->call(__FUNCTION__)
+            ->with($feedxml)
+            ->returning(DT::TEXT);
+        }
+        else
+        {
+            return (bool) is_atom($feedxml);
+        }
     }
 
     protected function is_rss($feedxml)
     {
-        //return is_rss($feedxml);
-        return (string) self::parameters(
-        [
-            'feedxml' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($feedxml)
-        ->returning(DT::TEXT);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (bool) self::parameters(
+            [
+                'feedxml' => DT::TEXT
+            ])
+            ->call(__FUNCTION__)
+            ->with($feedxml)
+            ->returning(DT::BOOL);
+        }
+        else
+        {
+            return (bool) is_rss($feedxml);
+        }
     }
 
     protected function is_xml(&$response)
     {
-        //return is_xml($response);
-        return (string) self::parameters(
-        [
-            'response' => DT::TEXT
-        ])
-        ->call(__FUNCTION__)
-        ->with($response)
-        ->returning(DT::TEXT);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (bool) self::parameters(
+            [
+                'response' => DT::TEXT
+            ])
+            ->call(__FUNCTION__)
+            ->with($response)
+            ->returning(DT::BOOL);
+        }
+        else
+        {
+            return (bool) is_xml($response);
+        }
     }
 
     protected function remove_parent_xml_node(&$xml, $node)
     {
-        //return remove_parent_xml_node($xml, $node);
-        return self::parameters(
-        [
-            'xml'  => DT::TEXT,
-            'node' => DT::STRING
-        ])
-        ->call(__FUNCTION__)
-        ->with($xml, $node)
-        ->returning(DT::VOID);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return self::parameters(
+            [
+                'xml'  => DT::TEXT,
+                'node' => DT::STRING
+            ])
+            ->call(__FUNCTION__)
+            ->with($xml, $node)
+            ->returning(DT::VOID);
+        }
+        else
+        {
+            return remove_parent_xml_node($xml, $node);
+        }
     }
 
     protected function xml_to_array($xml_object)
     {
-        //return xml_to_array($xml_object);
-        return (array) self::parameters(
-        [
-            'xml_object' => DT::TYPE_ARRAY
-        ])
-        ->call(__FUNCTION__)
-        ->with($xml_object)
-        ->returning(DT::TYPE_ARRAY);
+        if (defined('STRICT_TYPES') && CAMEL_CASE == '1')
+        {
+            return (array) self::parameters(
+            [
+                'xml_object' => DT::TYPE_ARRAY
+            ])
+            ->call(__FUNCTION__)
+            ->with($xml_object)
+            ->returning(DT::TYPE_ARRAY);
+        }
+        else
+        {
+            return (array) xml_to_array($xml_object);
+        }
     }
 }
