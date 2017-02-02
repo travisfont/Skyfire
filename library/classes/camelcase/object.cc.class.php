42560334c262a025d9cfa8a48f52328d913ce357
Travis van der Font <travis.font@gmail.com>
2017-02-02 23:12:06 +0100

    renaming Objects library to Object

--- library/classes/camelcase/object.cc.class.php
+++ library/classes/camelcase/object.cc.class.php
@@ -2,7 +2,7 @@
 
 // Object interface and function into object inherence
 
-class Objects extends Controller
+class Object extends Controller
 {
     // convertObjectsToArrays
     protected function convertObjectsToArrays($obj, &$arr)
