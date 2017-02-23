07368391806c4ccd3cb609aeab7014c0dca31e74
Travis van der Font <travis.font@gmail.com>
2017-02-23 16:05:51 +0100

    update to scale_image_to_width() - add casting on return

--- library/functions/image/scale_image_to_width.func.php
+++ library/functions/image/scale_image_to_width.func.php
@@ -5,5 +5,5 @@ function scale_image_to_width($filename, $target_width)
     $size = getimagesize($filename);
     $target_height = $target_width * ($size[1] / $size[0]);
 
-    return $target_height;
+    return (float) $target_height;
 }
