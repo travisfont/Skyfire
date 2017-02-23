b4f92febe60616f9c153e31ddab166edc2534e24
Travis van der Font <travis.font@gmail.com>
2017-02-23 16:06:16 +0100

    update to scale_image_to_height() - add casting on return

--- library/functions/image/scale_image_to_height.func.php
+++ library/functions/image/scale_image_to_height.func.php
@@ -5,5 +5,5 @@ function scale_image_to_height($filename, $target_height)
     $size = getimagesize($filename);
     $target_width = $target_height * ($size[0] / $size[1]);
 
-    return $target_width;
+    return (float) $target_width;
 }
