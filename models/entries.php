6f7281467edeea5a90fdc39840a6e0b1e0518312
Travis van der Font <travis.font@gmail.com>
2017-06-16 12:16:47 +0200

    -

--- models/entries.php
+++ models/entries.php
@@ -5,6 +5,6 @@ class Entries extends Model
     public static function getData()
     {
         //return self::Entries()->DB->select('get.AllHomeTextData')->execute();
-        return DB::select('get.AllHomeTextData')->execute();
+        return DB::select('get.tableinfo')->execute();
     }
 }
